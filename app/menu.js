d6f8217f48c6d0a7820cc7c18a5908591deb8062
Jas Laferriere <Fizzi36@gmail.com>
2019-01-10 21:53:12 -0500

    clean up help menu

--- app/menu.js
+++ app/menu.js
@@ -112,7 +112,7 @@ export default class MenuBuilder {
       subMenuEdit,
       subMenuView,
       subMenuWindow,
-      subMenuHelp
+      subMenuHelp,
     ];
   }
 
@@ -160,25 +160,10 @@ export default class MenuBuilder {
       label: 'Help',
       submenu: [{
         label: 'Learn More',
-        click() {
-          shell.openExternal('http://electron.atom.io');
-        }
-      }, {
-        label: 'Documentation',
-        click() {
-          shell.openExternal('https://github.com/atom/electron/tree/master/docs#readme');
-        }
-      }, {
-        label: 'Community Discussions',
-        click() {
-          shell.openExternal('https://discuss.atom.io/c/electron');
-        }
-      }, {
-        label: 'Search Issues',
-        click() {
-          shell.openExternal('https://github.com/atom/electron/issues');
-        }
-      }]
+        click: () => {
+          shell.openExternal('https://github.com/project-slippi/project-slippi');
+        },
+      }],
     }];
 
     return templateDefault;
