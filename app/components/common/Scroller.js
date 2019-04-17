9b2cb995758b28987e3a6b31ae30759b7634b7e3
Jessica Bunyan <tristan@bunyan.com>
2019-04-17 19:09:00 +0100

    proptypes for scroller

--- app/components/common/Scroller.js
+++ app/components/common/Scroller.js
@@ -1,11 +1,12 @@
-import _ from 'lodash';
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styles from './Scroller.scss';
 
-const path = require('path');
-
 export default class Scroller extends Component {
+  static propTypes = {
+    children: PropTypes.any.isRequired,
+  };
+
   render() {
     return <div className={styles['scroller']}>{this.props.children}</div>;
   }
