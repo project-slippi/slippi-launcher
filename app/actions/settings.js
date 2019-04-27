bd99e7271a8f89b99e2fee921c1922523a2b4e0e
Nikhil Narayana <nikhil.narayana@live.com>
2019-04-27 13:34:40 -0400

    additional ISO and small comment fixes
    Signed-off-by: Nikhil Narayana <nikhil.narayana@live.com>

--- app/actions/settings.js
+++ app/actions/settings.js
@@ -115,14 +115,15 @@ export function validateISO() {
     const hash = crypto.createHash('sha1');
     const input = fs.createReadStream(isoPath);
 
-    // Below are the sha1 hashes that are considered valid
+    // Below are the sha1 hashes for the ISOs we know about
     const ISOHashes = {
       "d4e70c064cc714ba8400a849cf299dbd1aa326fc": "success", // NTSC 1.02
-      "e63d50e63a0cdd357f867342d542e7cec0c3a7c7": "success", // 1.02 Scrubbed
+      "e63d50e63a0cdd357f867342d542e7cec0c3a7c7": "success", // 1.02 Scrubbed #1
+      "55109bc139b947c8b96b5fc913fbd91245104db8": "success", // 1.02 Scrubbed #2
       "2f0bed5e1d92ebb187840c6e1a2f368ce35f6816": "fail", // 20XX 3.02
       "7f6926f2f35940f5f697eb449c9f3fbd3639dd45": "fail", // 20XX 4.07++
       "49fd53b0a5eb0da9215846cd653ccc4c3548ec69": "fail", // 20XX 4.07++ UCF
-      "c89cb9b694f0f26ee07a6ee0a3633ba579e5fa12": "fail", // NTSC 1.02 (scrubbed?)
+      "c89cb9b694f0f26ee07a6ee0a3633ba579e5fa12": "fail", // NTSC 1.00 (scrubbed?)
       "5ecab83cd72c0ff515d750280f92713f19fa46f1": "fail", // NTSC 1.01
       "d0a925866379c546ceb739eeb780d011383cb07c": "fail", // PAL
       "fe23c91b63b0731ef727c13253b6a8c6757432ac": "fail", // JP 1.00
