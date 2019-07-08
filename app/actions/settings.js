9c67062b38f530b9c9cb2299a7d072896e4b2cce
meta <hosaka-corp@users.noreply.github.com>
2019-07-07 21:37:29 -0500

    track ntsc-j v1.02 configuration

--- app/actions/settings.js
+++ app/actions/settings.js
@@ -150,18 +150,19 @@ export function validateISO() {
 
     // Below are the sha1 hashes for the ISOs we know about
     const ISOHashes = {
-      "d4e70c064cc714ba8400a849cf299dbd1aa326fc": "success", // NTSC 1.02
-      "e63d50e63a0cdd357f867342d542e7cec0c3a7c7": "success", // 1.02 Scrubbed #1
-      "55109bc139b947c8b96b5fc913fbd91245104db8": "success", // 1.02 Scrubbed #2
+      "d4e70c064cc714ba8400a849cf299dbd1aa326fc": "success", // NTSC-U 1.02
+      "e63d50e63a0cdd357f867342d542e7cec0c3a7c7": "success", // NTSC-U 1.02 Scrubbed #1
+      "55109bc139b947c8b96b5fc913fbd91245104db8": "success", // NTSC-U 1.02 Scrubbed #2
+      "71255a30a47b4c6aabb90308d7a514d09d93a7b5": "success", // NTSC-J 1.02
       "2f0bed5e1d92ebb187840c6e1a2f368ce35f6816": "fail", // 20XX 3.02
       "7f6926f2f35940f5f697eb449c9f3fbd3639dd45": "fail", // 20XX 4.07++
       "49fd53b0a5eb0da9215846cd653ccc4c3548ec69": "fail", // 20XX 4.07++ UCF
-      "5ab1553a941307bb949020fd582b68aabebecb30": "fail", // NTSC 1.00 (possibly)
-      "c89cb9b694f0f26ee07a6ee0a3633ba579e5fa12": "fail", // NTSC 1.00 (scrubbed?)
-      "5ecab83cd72c0ff515d750280f92713f19fa46f1": "fail", // NTSC 1.01
+      "c89cb9b694f0f26ee07a6ee0a3633ba579e5fa12": "fail", // NTSC-U 1.00 (scrubbed?)
+      "5ab1553a941307bb949020fd582b68aabebecb30": "fail", // NTSC-U 1.00
+      "5ecab83cd72c0ff515d750280f92713f19fa46f1": "fail", // NTSC-U 1.01
       "d0a925866379c546ceb739eeb780d011383cb07c": "fail", // PAL
-      "fe23c91b63b0731ef727c13253b6a8c6757432ac": "fail", // JP 1.00
-      "f7ff7664b231042f2c0802041736fb9396a94b83": "fail", // JP 1.01
+      "fe23c91b63b0731ef727c13253b6a8c6757432ac": "fail", // NTSC-J 1.00
+      "f7ff7664b231042f2c0802041736fb9396a94b83": "fail", // NTSC-J 1.01
       "c7c0866fbe6d7ebf3b9c4236f4f32f4c8f65b578": "fail", // Taikenban (demo)
     };
 
