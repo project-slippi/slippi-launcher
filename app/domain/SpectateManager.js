332003bbc8e14bad792337249268b0d539b8ffeb
Jas Laferriere <Fizzi36@gmail.com>
2020-09-20 13:49:27 -0700

    fix bug with loading past games

--- app/domain/SpectateManager.js
+++ app/domain/SpectateManager.js
@@ -17,11 +17,16 @@ const SLIPPI_WS_SERVER = process.env.SLIPPI_WS_SERVER;
 // eslint-disable-next-line import/prefer-default-export
 export class SpectateManager {
   constructor() {
+    this.prevChannelId = null;
     this.wsConnection = null;
     this.channels = [];
 
     // A connection can mirror its received gameplay
     this.dolphinManager = new DolphinManager(`spectate`, { mode: 'mirror' });
+    this.dolphinManager.on('dolphin-closed', () => {
+      // Clear previous channel ID when Dolphin closes
+      this.prevChannelId = null;
+    });
 
     // Initialize SlpFileWriter for writting files
     const slpSettings = {
@@ -110,11 +115,22 @@ export class SpectateManager {
       return;
     }
 
+    if (channelId === this.prevChannelId) {
+      // If we have not changed channels, don't do anything. Worth noting that closing
+      // dolphin will count as a channel change because it resets prevChannelId
+      return;
+    }
+
     this.wsConnection.sendUTF(JSON.stringify({
       type: "change-channel",
       channelId: channelId,
     }));
 
-    this.dolphinManager.startPlayback();
+    // Play an empty file such that we just launch into the waiting for game screen, this is
+    // used to clear out any previous file that we were reading for. The file will get updated
+    // by the fileWriter
+    this.dolphinManager.playFile("", true);
+
+    this.prevChannelId = channelId;
   }
 }
