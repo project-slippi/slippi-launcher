014ee8021d2de8340b0d7d7b43115fb069c2d675
Nikhil Narayana <nikhil.narayana@live.com>
2020-11-03 10:32:40 -0800

    pick the correct file when loading the GameProfile

--- app/containers/GameProfilePage.js
+++ app/containers/GameProfilePage.js
@@ -7,7 +7,8 @@ import { dismissError } from "../actions/error";
 
 function mapStateToProps(state) {
   const index = state.fileLoader.statsGameIndex;
-  const file = state.fileLoader.files[index] || null;
+  const files = state.fileLoader.filterReplays ? state.fileLoader.files : state.fileLoader.allFiles;
+  const file = files[index] || null;
   return {
     store: state.game,
     statsGameIndex: index,
