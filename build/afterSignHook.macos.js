db4b6e0e1d9b304593f3cf86f1f854fb1ea3b6de
Ryan McGrath <ryan@rymc.io>
2020-11-24 22:35:48 -0800

    First pass at signing the desktop app for macOS

---
+++ build/afterSignHook.macos.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const path = require('path');
+var electron_notarize = require('electron-notarize');
+
+module.exports = async function (params) {
+    if(process.platform !== 'darwin') {
+        return;
+    }
+
+    console.log('afterSign hook triggered', params);
+
+    // Bail early if this is a fork-caused PR build, which doesn't get
+    // secrets.
+    if(
+        !process.env.APPLE_TEAM_PROVIDER_ID || 
+        !process.env.APPLE_API_KEY || 
+        !process.env.APPLE_ISSUER_ID
+    ) {
+        console.log("Bailing, no secrets found.");
+        return;
+    }
+
+    let appId = 'com.github.projectslippi.slippidesktopapp';
+    let appPath = path.join(params.appOutDir, `${params.packager.appInfo.productFilename}.app`);
+    if (!fs.existsSync(appPath)) {
+        throw new Error(`Cannot find application at: ${appPath}`);
+    }
+
+    console.log(`Notarizing ${appId} found at ${appPath} (this could take awhile, get some coffee...)`);
+
+    try {
+        await electron_notarize.notarize({
+            appBundleId: appId,
+            appPath: appPath,
+            ascProvider: process.env.APPLE_TEAM_PROVIDER_ID,
+            appleApiKey: process.env.APPLE_API_KEY,
+            appleApiIssuer: process.env.APPLE_ISSUER_ID,
+        });
+
+        console.log(`Successfully notarized ${appId}`);
+    } catch (error) {
+        console.error(error);
+    }
+};
