794ba9ac724fc82d3ede36c0ac4c15347f345c85
Vince Au <vinceau09@gmail.com>
2020-12-07 11:20:20 +1100

    Initial boilerplate from electron-webpack-quick-start

--- app/store/configureStore.js
+++
@@ -1,9 +0,0 @@
-import configureStoreDev from './configureStore.dev';
-import configureStoreProd from './configureStore.prod';
-
-const isProd = process.env.NODE_ENV === 'production';
-const selectedConfigureStore = isProd ? configureStoreProd : configureStoreDev;
-
-export const { configureStore } = selectedConfigureStore;
-
-export const { history } = selectedConfigureStore;
--- app/utils/number.js
+++
@@ -1,9 +0,0 @@
-export function toOrdinal(n) {
-  const s = ['th', 'st', 'nd', 'rd'];
-  const v = n % 100;
-  return n + (s[(v - 20) % 10] || s[v] || s[0]);
-}
-
-export function formatPercent(n, fractionalDigits) {
-  return `${(n * 100).toFixed(fractionalDigits)}%`;
-}
