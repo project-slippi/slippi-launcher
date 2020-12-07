83d369d800828e4e684c02abeab15af7a2e2a751
Vince Au <vinceau09@gmail.com>
2020-12-07 21:55:11 +1100

    Fix refreshing of router paths and invalid hook call errors

--- custom.webpack.additions.js
+++ custom.webpack.additions.js
@@ -1,8 +1,18 @@
+const pkg = require("./package.json");
 const Dotenv = require("dotenv-webpack");
 
 module.exports = function (context) {
-  // Add dotenv plugin
+  // Expose dotenv variables
   context.plugins.push(new Dotenv());
+
+  // Fix refresh not working in react-router-dom
+  // For more info: https://stackoverflow.com/a/43212553
+  context.output.publicPath = "/";
+  context.devServer.historyApiFallback = true;
+
+  // Ensure all dependencies are marked as external.
+  // Without this, we randomly get "Invalid hook call" errors.
+  context.externals.push(...Object.keys(pkg.dependencies || {}));
 
   return context;
 };
