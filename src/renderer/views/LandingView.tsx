2ed2dcb9f0fffebe0a0220d90901482a40e96188
Vince Au <vinceau09@gmail.com>
2020-12-13 22:21:01 +1100

    Add Slippi logo to landing view

--- src/renderer/views/LandingView.tsx
+++ src/renderer/views/LandingView.tsx
@@ -1,9 +1,12 @@
+import styled from "styled-components";
 import React from "react";
 import { AppContext } from "@/store";
 import { QuickStart } from "@/containers/QuickStart/QuickStart";
 import Box from "@material-ui/core/Box";
 import { ThemeProvider } from "@material-ui/core/styles";
 import { createMuiTheme } from "@material-ui/core/styles";
+
+import slippiLogo from "@/styles/images/slippi-logo.svg";
 
 const theme = createMuiTheme({
   palette: {
@@ -13,14 +16,30 @@ const theme = createMuiTheme({
   },
 });
 
+const OuterBox = styled(Box)`
+  position: relative;
+  &::before {
+    content: "";
+    background-image: url("${slippiLogo}");
+    background-size: 50%;
+    background-position: 110% 120%;
+    background-repeat: no-repeat;
+    position: absolute;
+    top: 0;
+    height: 100%;
+    width: 100%;
+    opacity: 0.2;
+  }
+`;
+
 export const LandingView: React.FC = () => {
   const { state } = React.useContext(AppContext);
 
   return (
-    <Box display="flex" style={{ height: "100%", width: "100%" }}>
+    <OuterBox display="flex" style={{ height: "100%", width: "100%" }}>
       <ThemeProvider theme={theme}>
         <QuickStart user={state.user} />
       </ThemeProvider>
-    </Box>
+    </OuterBox>
   );
 };
