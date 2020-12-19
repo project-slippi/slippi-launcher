4f58e9a3fab0e62e617ccc9855c1c3f91b05555f
Vince Au <vinceau09@gmail.com>
2020-12-19 18:50:33 +1100

    Only set public path in development

--- custom.webpack.additions.js
+++ custom.webpack.additions.js
@@ -5,10 +5,12 @@ module.exports = function (context) {
   // Expose dotenv variables
   context.plugins.push(new Dotenv());
 
-  // Fix refresh not working in react-router-dom
+  // Fix refresh not working in react-router-dom in development
   // For more info: https://stackoverflow.com/a/43212553
-  context.output.publicPath = "/";
-  context.devServer.historyApiFallback = true;
+  if (process.env.NODE_ENV !== "production" && context.devServer) {
+    context.output.publicPath = "/";
+    context.devServer.historyApiFallback = true;
+  }
 
   // Ensure all dependencies are marked as external.
   // Without this, we randomly get "Invalid hook call" errors.
