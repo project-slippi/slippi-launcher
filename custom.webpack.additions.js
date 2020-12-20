8b00275c9f22762744789738c18761cad5db0756
Vince Au <vinceau09@gmail.com>
2020-12-20 12:10:59 +1100

    Remove unused webpack config.
    Since we switched to HashRouter instead of BrowserRouter in 24109d2 this
    configuration isn't really needed. Refresh still doesn't work but at
    least it doesn't error anymore.

--- custom.webpack.additions.js
+++ custom.webpack.additions.js
@@ -5,13 +5,6 @@ module.exports = function (context) {
   // Expose dotenv variables
   context.plugins.push(new Dotenv());
 
-  // Fix refresh not working in react-router-dom in development
-  // For more info: https://stackoverflow.com/a/43212553
-  if (process.env.NODE_ENV !== "production" && context.devServer) {
-    context.output.publicPath = "/";
-    context.devServer.historyApiFallback = true;
-  }
-
   // Ensure all dependencies are marked as external.
   // Without this, we randomly get "Invalid hook call" errors.
   context.externals.push(...Object.keys(pkg.dependencies || {}));
