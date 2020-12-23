9100f1d6d7b5a51ccdfc6f382883e3f313670f86
Vince Au <vinceau09@gmail.com>
2020-12-24 00:04:15 +1100

    Allow game metadata to be empty

--- src/common/replayBrowser/processGame.ts
+++ src/common/replayBrowser/processGame.ts
@@ -6,7 +6,6 @@ import { FileResult } from "./types";
 import { fileToDateAndTime } from "../time";
 
 export function processGame(fullPath: string): FileResult {
-  log.info(`Processing game: ${fullPath}`);
   const filename = path.basename(fullPath);
 
   const result: FileResult = {
@@ -22,31 +21,34 @@ export function processGame(fullPath: string): FileResult {
   try {
     const game = new SlippiGame(result.fullPath);
 
-    // Preload settings
+    // Load settings
     const settings = game.getSettings();
     result.settings = settings;
     if (_.isEmpty(settings.players)) {
       throw new Error("Game settings could not be properly loaded.");
     }
 
-    // Preload metadata
+    // Load metadata
     const metadata = game.getMetadata();
-    result.metadata = metadata;
-    if (metadata && metadata.lastFrame !== undefined) {
-      result.lastFrame = metadata.lastFrame;
-    }
+    if (metadata) {
+      result.metadata = metadata;
 
-    const startAtTime = fileToDateAndTime(
-      metadata.startAt,
-      filename,
-      result.fullPath
-    );
+      if (metadata && metadata.lastFrame !== undefined) {
+        result.lastFrame = metadata.lastFrame;
+      }
 
-    if (startAtTime) {
-      result.startTime = startAtTime.toISOString();
+      const startAtTime = fileToDateAndTime(
+        metadata.startAt,
+        filename,
+        result.fullPath
+      );
+
+      if (startAtTime) {
+        result.startTime = startAtTime.toISOString();
+      }
     }
   } catch (err) {
-    log.error(err);
+    log.error(`Error processing: ${fullPath}: ${err}`);
     result.hasError = true;
   }
 
