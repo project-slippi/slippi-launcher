dce53b0afe701b4db0e00cd7c1659cc4c7bd381e
Vince Au <vinceau09@gmail.com>
2020-12-24 23:39:04 +1100

    Use the global variable __static for resolving static files

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -8,3 +8,8 @@ declare module "*.tiff";
 
 declare module "@material-ui/icons/*";
 declare module "@material-ui/lab/*";
+
+// This is a globally accessible reference to the static folder
+// provided by electron-webpack.
+// For more info see: https://webpack.electron.build/using-static-assets
+declare const __static: string;
--- src/renderer/lib/utils.ts
+++ src/renderer/lib/utils.ts
@@ -1,18 +1,15 @@
-import * as path from "path";
-import * as url from "url";
-
-import { remote } from "electron";
+import path from "path";
+import url from "url";
 
 const isDevelopment = process.env.NODE_ENV !== "production";
 
-// see https://github.com/electron-userland/electron-webpack/issues/99#issuecomment-459251702
+// Fix static folder access in development. For more information see:
+// https://github.com/electron-userland/electron-webpack/issues/99#issuecomment-459251702
 export const getStatic = (val: string): string => {
   if (isDevelopment) {
     return url.resolve(window.location.origin, val);
   }
-  const appPath = remote.app.getAppPath();
-  const imagePath = path.join(appPath, "../static");
-  return path.resolve(path.join(imagePath, val));
+  return path.resolve(path.join(__static, val));
 };
 
 export const getCharacterIcon = (
