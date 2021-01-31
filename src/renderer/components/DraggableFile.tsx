915a404b6d888575de6dfdb29554c4d56fe18c52
Vince Au <vinceau09@gmail.com>
2021-01-31 12:37:36 +1100

    Add prop type and docs to DraggableFile

--- src/renderer/components/DraggableFile.tsx
+++ src/renderer/components/DraggableFile.tsx
@@ -11,11 +11,17 @@ const DraggableLink = styled.a`
   -webkit-app-region: drag;
 `;
 
-export const DraggableFile: React.FC<{
+export interface DraggableFileProps {
   fullPath: string;
   className?: string;
   style?: React.CSSProperties;
-}> = ({ fullPath, children, className, style }) => {
+}
+
+/**
+ * DraggableFile accepts the `fullPath` prop and allows that file to be dragged into other contexts
+ * such as copied to a different folder or dragged into web-sites etc.
+ */
+export const DraggableFile: React.FC<DraggableFileProps> = ({ fullPath, children, className, style }) => {
   const handleDragStart = (e: React.DragEvent<HTMLAnchorElement>) => {
     e.preventDefault();
     ipcRenderer.send("ondragstart", fullPath);
