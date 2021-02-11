8f8e04c5c0d5d19cf1671745cf303f2ecd0a16ba
Nikhil Narayana <nikhil.narayana@live.com>
2021-02-11 12:19:19 -0800

    update some comments

--- src/main/dolphinManager.ts
+++ src/main/dolphinManager.ts
@@ -14,13 +14,13 @@ electronSettings.configure({
 });
 
 export interface ReplayCommunication {
-  mode: "normal" | "mirror" | "queue";
+  mode: "normal" | "mirror" | "queue"; // default normal
   replay?: string; // path to the replay if in normal or mirror mode
   startFrame?: number; // when to start watching the replay
   endFrame?: number; // when to stop watching the replay
   commandId?: string; // random string, doesn't really matter
   outputOverlayFiles?: boolean; // outputs gameStartAt and gameStation to text files (only works in queue mode)
-  isRealTimeMode?: boolean; // default true; keeps dolphin fairly close to real time (about 2-3 frames)
+  isRealTimeMode?: boolean; // default true; keeps dolphin fairly close to real time (about 2-3 frames); only relevant in mirror mode
   shouldResync?: boolean; // default true; disables the resync functionality
   rollbackDisplayMethod?: "off" | "normal" | "visible"; // default off; normal shows like a player experienced it, visible shows ALL frames (normal and rollback)
   queue?: ReplayQueueItem[];
@@ -35,7 +35,7 @@ export interface ReplayQueueItem {
 }
 
 interface DolphinInstance {
-  dolphinUseType: "playback" | "spectate" | "config" | "netplay"; // max instances: playback, config, netplay - 1 | spectate - "infinite"
+  dolphinUseType: "playback" | "spectate" | "config" | "netplay";
   index?: number; // should refer to the index in relation to the list of sources for spectating
   dolphin?: ChildProcessWithoutNullStreams;
   commFilePath?: string;
@@ -59,12 +59,13 @@ async function generateCommunicationFilePath(
   return commFileFullPath;
 }
 
-// dolphinManager should be in control of all dolphin instances that get opened for actual use.
+// DolphinManager should be in control of all dolphin instances that get opened for actual use.
 // This includes playing netplay, viewing replays, watching broadcasts (spectating), and configuring Dolphin.
 // The openDolphin function is exported so other parts of the app can do some checks.
 export class DolphinManager extends EventEmitter {
   private static instance: DolphinManager;
 
+  // max instances: playback, config, netplay - 1 | spectate - "infinite"
   private dolphinInstances: DolphinInstances = {
     playback: null,
     spectate: null,
@@ -85,8 +86,8 @@ export class DolphinManager extends EventEmitter {
     replayComm?: ReplayCommunication,
   ): Promise<void> {
     // I hate how this is done
-    const meleeISOPath = await electronSettings.get("settings.isoPath");
     const isoParams: string[] = [];
+    const meleeISOPath = await electronSettings.get("settings.isoPath");
     if (meleeISOPath?.toString()) {
       isoParams.push("-b", "-e", meleeISOPath.toString());
     }
