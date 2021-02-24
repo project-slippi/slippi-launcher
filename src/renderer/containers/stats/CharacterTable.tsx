01ba92052c5d0c800a4529cc0f3d2bd076544ead
Alvaro Sevilla <alvaro@chainalysis.com>
2021-02-24 17:41:15 +0000

    Add Player and Opponent stat tables

---
+++ src/renderer/containers/stats/CharacterTable.tsx
@@ -0,0 +1,67 @@
+import _ from "lodash";
+import React from "react";
+import { GlobalStats, MatchupAggregate } from "common/game";
+import * as T from "../ReplayFileStats/TableStyles";
+import { characters } from "@slippi/slippi-js";
+import { getCharacterIcon } from "@/lib/utils";
+
+const columnCount = 5;
+
+export const CharacterTable: React.FC<{ stats: GlobalStats }> = ({ stats }) => {
+  const opponent = false;
+
+  const renderHeaderPlayer = () => {
+    const headerText = `${opponent ? "Opponent" : "Player"} Characters`;
+    return (
+      <T.TableRow>
+        <T.TableHeaderCell colSpan={columnCount}>{headerText}</T.TableHeaderCell>
+      </T.TableRow>
+    );
+  };
+
+  const renderHeaderColumns = () => {
+    return (
+      <T.TableRow>
+        <T.TableHeaderCell>Character</T.TableHeaderCell>
+        <T.TableHeaderCell>Games</T.TableHeaderCell>
+        <T.TableHeaderCell>Winrate</T.TableHeaderCell>
+        {opponent ? <T.TableHeaderCell>Players</T.TableHeaderCell> : null}
+        <T.TableHeaderCell>Filters</T.TableHeaderCell>
+      </T.TableRow>
+    );
+  };
+
+  const renderRows = () => {
+    const charStats = stats.charIds;
+    return Object.keys(charStats)
+      .sort((a, b) => charStats[b].count - charStats[a].count)
+      .map((k) => generateCharacterRow(Number(k), charStats[k]));
+    // .slice(0,9)
+  };
+
+  const generateCharacterRow = (charId: number, agg: MatchupAggregate) => {
+    const name = characters.getCharacterShortName(charId);
+    const count = agg.count;
+    const winrate = ((agg.won / agg.count) * 100).toFixed(0);
+
+    return (
+      <T.TableRow key={`${charId}-${count}`}>
+        <T.TableCell>
+          <T.GrayableImage src={getCharacterIcon(charId, 0)} height={24} width={24} />
+          <div>{name}</div>
+        </T.TableCell>
+        <T.TableCell>{count}</T.TableCell>
+        <T.TableCell>{winrate}%</T.TableCell>
+        {opponent ? <T.TableCell>{agg.unique.length}</T.TableCell> : null}
+      </T.TableRow>
+    );
+  };
+
+  return (
+    <T.Table>
+      {renderHeaderPlayer()}
+      {renderHeaderColumns()}
+      {renderRows()}
+    </T.Table>
+  );
+};
---
+++ src/renderer/containers/stats/OpponentTable.tsx
@@ -0,0 +1,62 @@
+import _ from "lodash";
+import React from "react";
+import * as T from "../ReplayFileStats/TableStyles";
+
+import { getCharacterIcon } from "@/lib/utils";
+import { GlobalStats, MatchupAggregate } from "common/game";
+
+export const OpponentTable: React.FC<{ stats: GlobalStats }> = ({ stats }) => {
+  const renderHeaderPlayer = () => {
+    const headerText = "Top Opponents";
+    return (
+      <T.TableRow>
+        <T.TableHeaderCell colSpan={5}>{headerText}</T.TableHeaderCell>
+      </T.TableRow>
+    );
+  };
+
+  const renderHeaderColumns = () => {
+    return (
+      <T.TableRow>
+        <T.TableHeaderCell>Player</T.TableHeaderCell>
+        <T.TableHeaderCell>Games</T.TableHeaderCell>
+        <T.TableHeaderCell>Winrate</T.TableHeaderCell>
+        <T.TableHeaderCell>Characters</T.TableHeaderCell>
+        <T.TableHeaderCell>Filters</T.TableHeaderCell>
+      </T.TableRow>
+    );
+  };
+
+  const renderRows = () => {
+    const oppStats = stats.opponents;
+    return Object.keys(oppStats)
+      .sort((a, b) => oppStats[b].count - oppStats[a].count)
+      .slice(0, 26)
+      .map((k) => generateOpponentRow(k, oppStats[k]));
+  };
+
+  const generateOpponentRow = (playerTag: string, agg: MatchupAggregate) => {
+    const chars = agg.charIds.map((charId: number) => (
+      <T.GrayableImage key={charId} src={getCharacterIcon(charId, 0)} height={24} width={24} />
+    ));
+
+    return (
+      <T.TableRow key={`${playerTag}-${agg.count}`}>
+        <T.TableCell>{playerTag}</T.TableCell>
+        <T.TableCell>{agg.count}</T.TableCell>
+        <T.TableCell>{((agg.won / agg.count) * 100).toFixed(0)}%</T.TableCell>
+        <T.TableCell>
+          <div>{chars.slice(0, 5)}</div>
+        </T.TableCell>
+      </T.TableRow>
+    );
+  };
+
+  return (
+    <T.Table>
+      {renderHeaderPlayer()}
+      {renderHeaderColumns()}
+      {renderRows()}
+    </T.Table>
+  );
+};
