6a292754f235db7e3ccd15d468da74159ce8bdc9
Vince Au <vinceau09@gmail.com>
2021-02-28 10:20:31 +1100

    chore(deps): add worker threads

--- webpack.main.additions.js
+++ webpack.main.additions.js
@@ -1,8 +1,33 @@
+const path = require("path");
+const ThreadsPlugin = require("threads-plugin");
 const Dotenv = require("dotenv-webpack");
 
 module.exports = function (context) {
-  // Expose dotenv variables
-  context.plugins.push(new Dotenv());
+  context.optimization.minimizer = [];
+
+  context.resolve.modules = [path.resolve(__dirname, "./src"), "node_modules"];
+
+  context.module.rules = context.module.rules.filter((r) => {
+    return r.use !== "node-loader";
+  });
+
+  context.module.rules.unshift({
+    test: /\.node$/,
+    use: {
+      loader: "native-ext-loader",
+      options: {
+        rewritePath: undefined,
+        name: "[path][name].[ext]",
+      },
+    },
+  });
+
+  context.plugins.unshift(
+    // Add threads worker support
+    new ThreadsPlugin({ target: "electron-node-worker" }),
+    // Expose dotenv variables
+    new Dotenv(),
+  );
 
   return context;
 };
