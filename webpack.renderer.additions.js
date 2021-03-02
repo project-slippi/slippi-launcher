c2f66031780a0795431db91541a244cb46f785ee
Vince Au <vinceau09@gmail.com>
2021-03-02 13:59:07 +1100

    chore(deps): remove old web-worker dependencies

--- webpack.renderer.additions.js
+++ webpack.renderer.additions.js
@@ -5,23 +5,6 @@ module.exports = function (context) {
   // Expose dotenv variables
   context.plugins.push(new Dotenv());
 
-  // Add web worker support. This needs to be processed before the other rules so use unshift.
-  context.module.rules.unshift({
-    test: /\.worker\.(js|ts)$/i,
-    use: [
-      {
-        loader: "comlink-loader",
-        options: {
-          singleton: true,
-        },
-      },
-    ],
-  });
-
-  // Fix web workers not working with HMR. Without this we get "window is not defined" errors.
-  // For more info: https://github.com/webpack/webpack/issues/6642
-  context.output.globalObject = "this";
-
   // Ensure all dependencies are marked as external.
   // Without this, we randomly get "Invalid hook call" errors.
   context.externals.push(...Object.keys(pkg.dependencies || {}));
