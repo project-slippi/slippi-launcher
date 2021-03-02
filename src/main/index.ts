415affb7e818903efabfa86153fca85b4b53a4a7
Vince Au <vinceau09@gmail.com>
2021-03-02 17:53:00 +1100

    feat: open external links in default browser by default

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,5 +1,5 @@
 import { colors } from "common/colors";
-import { app, BrowserWindow } from "electron";
+import { app, BrowserWindow, shell } from "electron";
 import contextMenu from "electron-context-menu";
 import * as path from "path";
 import { format as formatUrl } from "url";
@@ -53,6 +53,12 @@ function createMainWindow() {
     setImmediate(() => {
       window.focus();
     });
+  });
+
+  // Automatically open new-tab/new-window URLs in their default browser
+  window.webContents.on("new-window", (event: Event, url: string) => {
+    event.preventDefault();
+    shell.openExternal(url);
   });
 
   window.once("ready-to-show", () => {
