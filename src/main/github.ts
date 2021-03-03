7c1139fa439c58be81f7d1bf48c6cfdbb47abc9d
Vince Au <vinceau09@gmail.com>
2021-03-03 12:50:59 +1100

    feat: reuse github api calls

--- src/main/github.ts
+++ src/main/github.ts
@@ -15,9 +15,13 @@ interface ResponseCacheEntry {
 const githubResponseCache: Record<string, ResponseCacheEntry> = {};
 
 export async function getLatestRelease(owner: string, repo: string): Promise<any> {
-  const url = `https://api.github.com/repos/${owner}/${repo}/releases/latest`;
-  const data = await cachedFetch(url);
-  return data;
+  // We can re-use api calls by returning the first item in all releases
+  // since it already returns the newest release first.
+  const data = await getAllReleases(owner, repo);
+  if (data.length > 0) {
+    return data[0];
+  }
+  throw new Error(`No releases found for ${owner}/${repo}`);
 }
 
 export async function getAllReleases(owner: string, repo: string): Promise<any> {
@@ -27,7 +31,7 @@ export async function getAllReleases(owner: string, repo: string): Promise<any> 
 }
 
 async function cachedFetch(url: string): Promise<any> {
-  log.debug(`Fetching: ${url}`);
+  log.debug(`Checking cache for: ${url}`);
   const cachedResponse = githubResponseCache[url];
   if (cachedResponse) {
     // Check if the cache has expired
@@ -40,6 +44,7 @@ async function cachedFetch(url: string): Promise<any> {
     }
   }
 
+  log.debug(`Fetching: ${url}`);
   // Fetch the data
   const res = await fetch(url);
   const data = await res.json();
