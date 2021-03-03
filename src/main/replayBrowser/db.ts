96a259a05b8344d78e824c32acc93bca5919447b
Alvaro Sevilla <alvaro@chainalysis.com>
2021-03-03 02:58:36 +0000

    Separate db into two tables

--- src/main/replayBrowser/db.ts
+++ src/main/replayBrowser/db.ts
@@ -1,36 +1,36 @@
 import { FileResult } from "common/types";
 import { app } from "electron";
 import path from "path";
-import * as sqlite3 from "sqlite3";
+import { Database, open } from "sqlite";
+import { Database as Sql3Db } from "sqlite3";
 
-const db = new sqlite3.Database(path.join(app.getPath("userData"), "sqlippi.db"), (err) => {
-  if (err) {
-    return console.error(err.message);
-  }
+let db: Database | undefined = undefined;
+
+(async () => {
+  // open the database
+  db = await open({
+    filename: path.join(app.getPath("userData"), "sqlippi.db"),
+    driver: Sql3Db,
+  });
   console.log("Connected to the replay database.");
-  db.run(`
-  CREATE TABLE IF NOT EXISTS replays (
+  await db.exec(`
+    CREATE TABLE IF NOT EXISTS replays (
        fullPath      TEXT PRIMARY KEY,
        name          TEXT,
        folder        TEXT,
-       startTime     TEXT,
+       startTime     TEXT)
+  `);
+  await db.exec("CREATE INDEX IF NOT EXISTS folder_idx ON replays(folder)");
+  await db.exec(`
+    CREATE TABLE IF NOT EXISTS replay_data (
+       fullPath      TEXT PRIMARY KEY,
        lastFrame     INTEGER,
-       playerCount   INTEGER,
-       player1       INTEGER,
-       player2       INTEGER,
-       player3       INTEGER,
-       player4       INTEGER,
        settings      JSON,
        metadata      JSON,
-       stats         JSON)
+       stats         JSON,
+       FOREIGN KEY (fullPath) REFERENCES replays(fullPath) ON DELETE CASCADE);
   `);
-
-  db.run("CREATE INDEX IF NOT EXISTS folder_idx  ON replays(folder)");
-  db.run("CREATE INDEX IF NOT EXISTS player1_idx ON replays(player1)");
-  db.run("CREATE INDEX IF NOT EXISTS player2_idx ON replays(player2)");
-  db.run("CREATE INDEX IF NOT EXISTS player3_idx ON replays(player3)");
-  db.run("CREATE INDEX IF NOT EXISTS player4_idx ON replays(player4)");
-});
+})();
 
 const parseRow = (row: any) => {
   return {
@@ -41,82 +41,53 @@ const parseRow = (row: any) => {
     lastFrame: row.lastFrame,
     metadata: JSON.parse(row.metadata),
     stats: JSON.parse(row.stats || null),
-    playerCount: row.playerCount,
-    player1: row.player1,
-    player2: row.player2,
-    player3: row.player3,
-    player4: row.player4,
     folder: row.folder,
   } as FileResult;
 };
 
-export const getFolderFiles = (folder: string) => {
-  return new Promise<string[]>((resolve, reject) =>
-    db.all(
-      `
-    SELECT fullPath
-    FROM replays 
-    WHERE folder = ?
-    ORDER by startTime DESC`,
-      [folder],
-      (err, files) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(files ? files.map((f) => f.fullPath) : []);
-        }
-      },
-    ),
+export const getFolderFiles = async (folder: string) => {
+  const files = await db!.all(
+    `
+      SELECT fullPath
+      FROM replays 
+      WHERE folder = ?
+      ORDER by startTime DESC`,
+    [folder],
   );
+  return files ? files.map((f) => f.fullPath) : [];
 };
 
-export const getFolderReplays = (folder: string) => {
-  return new Promise<FileResult[]>((resolve, reject) =>
-    db.all(
-      `
+export const getFolderReplays = async (folder: string) => {
+  const docs = await db!.all(
+    `
     SELECT fullPath, name, folder, startTime, lastFrame, 
-    playerCount, player1, player2, player3, player4, 
     settings, metadata 
     FROM replays 
+    JOIN replay_data USING (fullPath)
     WHERE folder = ?
     ORDER by startTime DESC`,
-      [folder],
-      (err, docs) => {
-        if (err) {
-          reject(err);
-        } else {
-          const files = docs.map(parseRow);
-          resolve(docs ? files : []);
-        }
-      },
-    ),
+    [folder],
   );
+  const files = docs.map(parseRow);
+  return docs ? files : [];
 };
 
-export const getFullReplay = (file: string) => {
-  return new Promise<FileResult>((resolve, reject) =>
-    db.all("SELECT * from replays WHERE fullPath = ?", [file], (err, docs) => {
-      if (err) {
-        reject(err);
-      } else {
-        const files = docs.map(parseRow);
-        resolve(files[0]);
-      }
-    }),
-  );
+export const getFullReplay = async (file: string) => {
+  const doc = await db!.get("SELECT * from replays JOIN replay_data USING (fullPath) WHERE fullPath = ?", [file]);
+  return parseRow(doc);
 };
 
-export const getPlayerReplays = (player: string) => {
-  return new Promise((resolve, reject) =>
-    db.all("SELECT * from replays WHERE (?) IN (player1, player2, player3, player4)", [player], (err, docs) => {
-      if (err) {
-        reject(err);
-      } else {
-        const files = docs.map(parseRow);
-        resolve(docs ? files[0] : []);
-      }
-    }),
-  );
+export const getPlayerReplays = async (_: string) => {
+  // return new Promise((resolve, reject) =>
+  //   db.all("SELECT * from replays WHERE (?) IN (player1, player2, player3, player4)", [player], (err, docs) => {
+  //     if (err) {
+  //       reject(err);
+  //     } else {
+  //       const files = docs.map(parseRow);
+  //       resolve(docs ? files[0] : []);
+  //     }
+  //   }),
+  // );
 };
 
 export const saveReplays = async (replays: FileResult[]) => {
@@ -130,54 +101,51 @@ export const saveReplays = async (replays: FileResult[]) => {
   }
 };
 
+const inTransaction = async (cb: () => Promise<void>) => {
+  db!.exec("BEGIN");
+  try {
+    await cb();
+    db!.exec("COMMIT");
+  } catch (err) {
+    db!.exec("ROLLBACK");
+    throw err;
+  }
+};
+
 const saveReplayBatch = async (replays: FileResult[]) => {
-  return new Promise((resolve, reject) => {
-    const placeholders = replays.map(() => "(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)").join(",");
-    db.run(
+  await inTransaction(async () => {
+    const placeholdersMeta = replays.map(() => "(?, ?, ? ,?)").join(",");
+    await db!.run(
       `
-       INSERT INTO replays(
-       fullPath, name, folder, startTime, lastFrame, 
-       playerCount, player1, player2, player3, player4, 
-       settings, metadata, stats)
-       VALUES ` + placeholders,
+      INSERT INTO replays(
+      fullPath, name, folder, startTime)
+      VALUES ` + placeholdersMeta,
+      replays.flatMap((replay: FileResult) => [replay.fullPath, replay.name, replay.folder, replay.startTime]),
+    );
+    const placeholdersData = replays.map(() => "(?, ?, ?, ?, ?)").join(",");
+    await db!.run(
+      `
+      INSERT INTO replay_data(
+      fullPath, lastFrame, 
+      settings, metadata, stats)
+      VALUES ` + placeholdersData,
       replays.flatMap((replay: FileResult) => [
         replay.fullPath,
-        replay.name,
-        replay.folder,
-        replay.startTime,
         replay.lastFrame,
-        replay.playerCount,
-        replay.player1,
-        replay.player2,
-        replay.player3,
-        replay.player4,
         JSON.stringify(replay.settings),
         JSON.stringify(replay.metadata),
         JSON.stringify(replay.stats),
       ]),
-      (err) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(null);
-        }
-      },
     );
   });
 };
 
-export const deleteReplays = (files: string[]) => {
+export const deleteReplays = async (files: string[]) => {
   const qfmt = files.map(() => "?").join(",");
-  return new Promise((resolve, reject) =>
-    db.run(`DELETE FROM replays WHERE fullPath IN (${qfmt})`, files, (err) => (err ? reject(err) : resolve(null))),
-  );
+  await db!.run(`DELETE FROM replays WHERE fullPath IN (${qfmt})`, files);
 };
 
-export const pruneFolders = (existingFolders: string[]) => {
+export const pruneFolders = async (existingFolders: string[]) => {
   const qfmt = existingFolders.map(() => "?").join(", ");
-  return new Promise((resolve, reject) =>
-    db.run(`DELETE FROM replays WHERE folder NOT IN (${qfmt})`, existingFolders, (err) =>
-      err ? reject(err) : resolve(null),
-    ),
-  );
+  await db!.run(`DELETE FROM replays WHERE folder NOT IN (${qfmt})`, existingFolders);
 };
