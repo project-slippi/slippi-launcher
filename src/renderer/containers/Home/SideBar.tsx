152d6a82358da4215ac9afbadc808ef90c26760b
Vince Au <vinceau09@gmail.com>
2021-03-16 22:37:08 +1100

    feat: add loading indicator to twitter feed

--- src/renderer/containers/Home/SideBar.tsx
+++ src/renderer/containers/Home/SideBar.tsx
@@ -1,23 +1,13 @@
 import React from "react";
-import { TwitterTimelineEmbed } from "react-twitter-embed";
 import styled from "styled-components";
 
 import { Advertisements } from "./Advertisements";
+import { TwitterFeed } from "./TwitterFeed";
 
 const Outer = styled.div`
   display: flex;
   flex-direction: column;
   flex: 1;
-`;
-
-const TwitterFeedContainer = styled.div`
-  height: 100%;
-  width: 100%;
-  overflow: auto;
-  & > div {
-    height: 100%;
-    width: 100%;
-  }
 `;
 
 const AdvertContainer = styled.div`
@@ -28,16 +18,7 @@ const AdvertContainer = styled.div`
 export const SideBar: React.FC = () => {
   return (
     <Outer>
-      <TwitterFeedContainer>
-        <TwitterTimelineEmbed
-          sourceType="profile"
-          screenName="projectslippi"
-          theme="dark"
-          noHeader={true}
-          noFooter={true}
-          transparent={true}
-        />
-      </TwitterFeedContainer>
+      <TwitterFeed />
       <AdvertContainer>
         <Advertisements />
       </AdvertContainer>
