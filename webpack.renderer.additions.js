b26639a96ccfadf0ddbf7801d477b9b7fdaf85f8
Vince Au <vinceau09@gmail.com>
2021-03-20 16:31:28 +1100

    chore: patch electron-webpack to ignore svg

--- webpack.renderer.additions.js
+++ webpack.renderer.additions.js
@@ -9,9 +9,6 @@ module.exports = function (context) {
   // Without this, we randomly get "Invalid hook call" errors.
   context.externals.push(...Object.keys(pkg.dependencies || {}));
 
-  // Ignore conflicting SVG rules
-  context.module.rules = context.module.rules.filter((rule) => !rule.test.toString().includes("svg"));
-
   // Allow importing raw SVGs
   context.module.rules.push({
     test: /\.svg$/,
