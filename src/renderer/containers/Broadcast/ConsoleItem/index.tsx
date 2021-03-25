84b86dde36aaea0b8d7018c6bfeebb79f3c141c5
Vince Au <vinceau09@gmail.com>
2021-03-26 02:46:29 +1100

    fix: windows case-insensitivity nonsense

---
+++ src/renderer/containers/Broadcast/ConsoleItem/index.tsx
@@ -0,0 +1,99 @@
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import Collapse from "@material-ui/core/Collapse";
+import { red } from "@material-ui/core/colors";
+import IconButton from "@material-ui/core/IconButton";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import ShareIcon from "@material-ui/icons/Share";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
+import WifiTetheringIcon from "@material-ui/icons/WifiTethering";
+import clsx from "clsx";
+import React from "react";
+
+import { ReactComponent as DolphinIcon } from "@/styles/images/dolphin.svg";
+
+import { StartBroadcastDialog } from "./StartBroadcastDialog";
+
+export interface ConsoleItemProps {
+  name: string;
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      maxWidth: 345,
+    },
+    media: {
+      height: 0,
+      paddingTop: "56.25%", // 16:9
+    },
+    expand: {
+      transform: "rotate(0deg)",
+      marginLeft: "auto",
+      transition: theme.transitions.create("transform", {
+        duration: theme.transitions.duration.shortest,
+      }),
+    },
+    expandOpen: {
+      transform: "rotate(180deg)",
+    },
+    avatar: {
+      backgroundColor: red[500],
+    },
+    actions: {
+      justifyContent: "flex-end",
+    },
+  }),
+);
+
+export const ConsoleItem: React.FC<ConsoleItemProps> = () => {
+  const [modalOpen, setModalOpen] = React.useState(false);
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const handleClose = () => setAnchorEl(null);
+
+  return (
+    <div>
+      <Card className={classes.root}>
+        <CardHeader
+          avatar={<DolphinIcon fill="white" height="40px" width="40px" />}
+          action={
+            <IconButton aria-label="settings" onClick={(e) => setAnchorEl(e.currentTarget)}>
+              <MoreVertIcon />
+            </IconButton>
+          }
+          title="Slippi Dolphin"
+          subheader="127.0.0.1:12345"
+        />
+        <Menu id="simple-menu" anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={handleClose}>
+          <MenuItem onClick={handleClose}>Configure</MenuItem>
+          <MenuItem onClick={handleClose}>Delete</MenuItem>
+        </Menu>
+        <CardActions disableSpacing className={classes.actions}>
+          <Button size="small" color="primary" onClick={() => setModalOpen(true)}>
+            Broadcast
+          </Button>
+        </CardActions>
+      </Card>
+      <StartBroadcastDialog
+        open={modalOpen}
+        onClose={() => setModalOpen(false)}
+        onSubmit={(userId) => console.log(`broadcasting to user: ${userId}`)}
+      />
+    </div>
+  );
+};
