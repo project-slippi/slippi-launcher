82a1c3f92ae64b2642fa8016cdc42b5ccae06f77
Kyle Reese <kylereese@kreesemac.local>
2021-03-28 18:32:06 -0400

    style the patchnote

--- src/renderer/containers/Home/NewsFeed/PatchNote.tsx
+++ src/renderer/containers/Home/NewsFeed/PatchNote.tsx
@@ -1,8 +1,16 @@
 import { Modal } from "@material-ui/core";
 import { NewsItem } from "common/types";
 import React from "react";
+import styled from "styled-components";
 
 import { NewsArticle } from "./NewsArticle";
+
+const Outer = styled.div`
+  width: 75%;
+  margin-top: 25%;
+  margin-left: 10%;
+  position: absolute;
+`;
 
 export interface PatchNoteProps {
   open: boolean;
@@ -11,20 +19,20 @@ export interface PatchNoteProps {
 }
 
 export const PatchNote: React.FC<PatchNoteProps> = (props) => {
+  //get most recent stored version ID
   const prevVer = localStorage.getItem("ver");
   React.useEffect(() => {
     // check if previous version exists - if it does and it differs from current version,
     // we display the current version as a PatchNote
     props.setOpen(prevVer == null ? true : prevVer === props.currVer.id ? false : true);
+    //store the current version
     localStorage.setItem("ver", props.currVer.id);
   }, []);
   return (
-    <div>
-      <Modal open={props.open} onClose={() => props.setOpen(false)}>
-        <div>
-          <NewsArticle key={props.currVer.id} item={props.currVer} />
-        </div>
-      </Modal>
-    </div>
+    <Modal open={props.open} onClose={() => props.setOpen(false)}>
+      <Outer>
+        <NewsArticle key={props.currVer.id} item={props.currVer} />
+      </Outer>
+    </Modal>
   );
 };
