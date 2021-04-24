582217c75b1650e16a249c7d376bbf31962620cf
Vince Au <vinceau09@gmail.com>
2021-04-24 23:53:23 +1000

    feat: add notice to login for user-required features

---
+++ src/renderer/components/LoginNotice.tsx
@@ -0,0 +1,30 @@
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import PersonOutlineIcon from "@material-ui/icons/PersonOutline";
+import React from "react";
+import styled from "styled-components";
+
+import { useLoginModal } from "@/lib/hooks/useLoginModal";
+
+export const LoginNotice: React.FC = () => {
+  const openModal = useLoginModal((store) => store.openModal);
+  return (
+    <Outer>
+      <div>
+        <PersonOutlineIcon style={{ fontSize: 100 }} />
+      </div>
+      <Typography variant="h6">User is not logged in</Typography>
+      <Button type="button" color="primary" variant="contained" style={{ textTransform: "none" }} onClick={openModal}>
+        Log in
+      </Button>
+    </Outer>
+  );
+};
+
+const Outer = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
