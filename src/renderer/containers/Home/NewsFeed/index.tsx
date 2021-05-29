5280adfec1e6db9cacfaef1e37c488226299cd5c
Vince Au <vinceau09@gmail.com>
2021-05-29 12:08:40 +1000

    Revert "feat: utilise useValue for fetching articles list"
    This reverts commit d425bd2a712a96e4cd2c7cd2687a932489fc075d.
    Reverting because it's actually nice to utilise useQuery's cache.

--- src/renderer/containers/Home/NewsFeed/index.tsx
+++ src/renderer/containers/Home/NewsFeed/index.tsx
@@ -1,6 +1,8 @@
 import Typography from "@material-ui/core/Typography";
 import { fetchNewsFeed } from "common/ipc";
+import log from "electron-log";
 import React from "react";
+import { useQuery } from "react-query";
 import styled from "styled-components";
 
 import { NewsArticle } from "./NewsArticle";
@@ -11,18 +13,26 @@ const Outer = styled.div`
 `;
 
 export const NewsFeed: React.FC = () => {
-  const articlesList = fetchNewsFeed.renderer!.useValue({}, []);
+  const newsFeedQuery = useQuery(["news-articles"], async () => {
+    const articlesResult = await fetchNewsFeed.renderer!.trigger({});
+    if (!articlesResult.result) {
+      log.error("NewsFeed: error fetching news articles", articlesResult.errors);
+      throw new Error("Error fetching news articles");
+    }
+    return articlesResult.result;
+  });
 
-  if (articlesList.isUpdating) {
+  if (newsFeedQuery.isLoading) {
     return <div>Loading...</div>;
   }
 
+  const posts = newsFeedQuery.data ?? [];
   return (
     <Outer>
       <Typography variant="h4" style={{ marginBottom: 20 }}>
         Latest News
       </Typography>
-      {articlesList.value.map((post) => (
+      {posts.map((post) => (
         <NewsArticle key={post.id} item={post} />
       ))}
     </Outer>
