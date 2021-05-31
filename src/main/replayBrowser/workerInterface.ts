58d620ac8270ab3ec00bfa94058fe2057a6b658e
Vince Au <vinceau09@gmail.com>
2021-05-31 17:34:59 +1000

    refactor: remove old code

--- src/main/replayBrowser/workerInterface.ts
+++ src/main/replayBrowser/workerInterface.ts
@@ -1,21 +1,3 @@
-// import { app } from "electron";
-// import log from "electron-log";
-// import os from "os";
-// import { Pool as spawnPool, spawn, Worker } from "threads";
-
-// import { WorkerSpec } from "./worker";
-
-// const workerPool = spawnPool(() => spawn<WorkerSpec>(new Worker("./worker")), os.cpus().length);
-// log.debug("replayBrowser: Spawning pool");
-
-// async function terminatePool() {
-//   log.debug("replayBrowser: Terminating pool");
-//   await workerPool.terminate();
-// }
-// app.on("quit", terminatePool);
-
-// export const loadReplayFile = async (fullPath: string) => await workerPool.queue((w) => w.loadReplayFile(fullPath));
-
 import { app } from "electron";
 import log from "electron-log";
 import { spawn, Thread, Worker } from "threads";
