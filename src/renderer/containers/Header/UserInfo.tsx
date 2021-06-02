bed67ace36f90220fef605584581c43dba64dd48
Vince Au <vinceau09@gmail.com>
2021-06-02 17:44:14 +1000

    feat: add avatar outline

--- src/renderer/containers/Header/UserInfo.tsx
+++ src/renderer/containers/Header/UserInfo.tsx
@@ -1,4 +1,7 @@
+/** @jsx jsx */
+import { css, jsx } from "@emotion/react";
 import Avatar from "@material-ui/core/Avatar";
+import { colors } from "common/colors";
 import firebase from "firebase";
 import React from "react";
 
@@ -14,7 +17,14 @@ export const UserInfo: React.FC<{
         alignItems: "center",
       }}
     >
-      <Avatar src={imageUrl} />
+      <Avatar
+        src={imageUrl}
+        css={css`
+          border: solid 3px ${colors.purpleLight};
+          height: 30px;
+          width: 30px;
+        `}
+      />
       <div style={{ marginLeft: 10, fontSize: 18 }}>{user.displayName}</div>
     </div>
   );
