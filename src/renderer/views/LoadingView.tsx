8683c3b0b1f3fbabcc3d228ad30e5ecc7836a201
Vince Au <vinceau09@gmail.com>
2021-06-02 17:32:31 +1000

    feat: add slippi background to loading screen

--- src/renderer/views/LoadingView.tsx
+++ src/renderer/views/LoadingView.tsx
@@ -1,9 +1,12 @@
+/** @jsx jsx */
+import { jsx } from "@emotion/react";
 import React from "react";
 
 import { LoadingScreen } from "@/components/LoadingScreen";
 import { useApp } from "@/store/app";
+import { withSlippiBackground } from "@/styles/withSlippiBackground";
 
 export const LoadingView: React.FC = () => {
   const installStatus = useApp((store) => store.logMessage);
-  return <LoadingScreen message={installStatus ? installStatus : "Just a sec..."} />;
+  return <LoadingScreen css={withSlippiBackground} message={installStatus ? installStatus : "Just a sec..."} />;
 };
