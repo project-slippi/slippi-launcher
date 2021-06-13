718be2f32c8252a59a10c1a23b0a859cec30b747
Vince Au <vinceau09@gmail.com>
2021-06-13 23:43:22 +1000

    fix: security issue with loading remote Twitter content
    Since we have nodeIntegration enabled, loading remote content such as
    the Twitter scripts for the embedded timeline is extremely dangerous.
    To fix this, we host the timeline on a remote server and use an iframe
    to show the contents.

--- src/renderer/containers/Home/TwitterFeed.tsx
+++ src/renderer/containers/Home/TwitterFeed.tsx
@@ -1,11 +1,6 @@
 import styled from "@emotion/styled";
-import { colors } from "common/colors";
 import { socials } from "common/constants";
 import React from "react";
-import { TwitterTimelineEmbed } from "react-twitter-embed";
-
-import { ErrorBoundary } from "@/components/ErrorBoundary";
-import { Spinner } from "@/components/Spinner";
 
 const TwitterFeedContainer = styled.div`
   transition: opacity 1s ease-in-out;
@@ -18,36 +13,21 @@ const TwitterFeedContainer = styled.div`
 const Outer = styled.div`
   position: relative;
   flex: 1;
-`;
-
-const LoadingIndicator = styled.div`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+  overflow: hidden;
 `;
 
 export const TwitterFeed: React.FC = () => {
-  const [isLoading, setIsLoading] = React.useState(true);
   return (
-    <Outer style={{ overflow: isLoading ? "hidden" : "auto" }}>
-      <LoadingIndicator style={{ display: isLoading ? "block" : "none" }}>
-        <Spinner />
-      </LoadingIndicator>
-      <ErrorBoundary>
-        <TwitterFeedContainer style={{ opacity: isLoading ? 0 : 1 }}>
-          <TwitterTimelineEmbed
-            sourceType="profile"
-            screenName={socials.twitterId}
-            theme="dark"
-            borderColor={colors.purpleLight}
-            noHeader={true}
-            noFooter={true}
-            transparent={true}
-            onLoad={() => setIsLoading(false)}
-          />
-        </TwitterFeedContainer>
-      </ErrorBoundary>
+    <Outer>
+      <TwitterFeedContainer style={{ height: "100%" }}>
+        <iframe
+          sandbox="allow-scripts allow-same-origin allow-popups"
+          src={`https://vinceau.github.io/twitter-embed/?screenName=${socials.twitterId}&theme=dark&transparent=true`}
+          height="100%"
+          width="100%"
+          frameBorder="none"
+        />
+      </TwitterFeedContainer>
     </Outer>
   );
 };
