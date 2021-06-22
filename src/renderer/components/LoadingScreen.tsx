6f20ad2af48dddac32ab7b5253cb3ce40ea73e5e
Vince Au <vinceau09@gmail.com>
2021-06-23 09:24:00 +1000

    feat: improve loading screen styles

--- src/renderer/components/LoadingScreen.tsx
+++ src/renderer/components/LoadingScreen.tsx
@@ -1,3 +1,5 @@
+/** @jsx jsx */
+import { css, jsx } from "@emotion/react";
 import React from "react";
 
 import { BouncingSlippiLogo } from "@/components/BouncingSlippiLogo";
@@ -11,7 +13,17 @@ export const LoadingScreen: React.FC<{
 }> = ({ message, style, className }) => {
   return (
     <Message className={className} style={style} icon={<BouncingSlippiLogo />}>
-      {message}
+      <p
+        css={css`
+          text-align: center;
+          max-width: 650px;
+          word-break: break-word;
+          line-height: 1.4em;
+          padding: 0 50px;
+        `}
+      >
+        {message}
+      </p>
     </Message>
   );
 };
