936c0e50752919de863a853f9e1f4f60ab1903e9
Vince Au <vinceau09@gmail.com>
2021-06-27 09:55:33 +1000

    fix: incorrect header colors on help page

--- src/renderer/containers/Settings/HelpPage.tsx
+++ src/renderer/containers/Settings/HelpPage.tsx
@@ -22,7 +22,10 @@ export const HelpPage: React.FC = () => {
 
           h1,
           h2,
-          h3 {
+          h3,
+          h4,
+          h5,
+          h6 {
             color: white;
           }
         `}
