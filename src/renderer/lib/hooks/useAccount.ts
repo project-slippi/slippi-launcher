b8a20013899ab7f09f1fdcc2691ae09deb3218be
Nikhil Narayana <nikhil.narayana@live.com>
2021-06-25 18:21:34 -0700

    refactor: handle changing display name in useAccount

--- src/renderer/lib/hooks/useAccount.ts
+++ src/renderer/lib/hooks/useAccount.ts
@@ -11,11 +11,21 @@ export const useAccount = create(
       user: null as firebase.User | null,
       loading: false,
       playKey: null as PlayKey | null,
+      displayName: "",
     },
     (set, get) => ({
-      setUser: (user: firebase.User | null) => set({ user }),
+      setUser: (user: firebase.User | null) =>
+        set((store) => {
+          store.user = user;
+          if (user) {
+            store.displayName = user.displayName || "";
+          }
+
+          return store;
+        }),
       setLoading: (loading: boolean) => set({ loading }),
       setPlayKey: (playKey: PlayKey | null) => set({ playKey }),
+      setDisplayName: (displayName: string) => set({ displayName }),
       refreshPlayKey: async (): Promise<void> => {
         // We're already refreshing the key
         if (get().loading) {
