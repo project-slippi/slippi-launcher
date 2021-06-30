e11fb11ad77a678a8a561fc0cea576ab642ca08c
Vince Au <vinceau09@gmail.com>
2021-06-30 18:24:31 +1000

    feat: don't use modal for importing dolphin settings

--- src/renderer/containers/Settings/DolphinSettings.tsx
+++ src/renderer/containers/Settings/DolphinSettings.tsx
@@ -9,15 +9,13 @@ import { DolphinLaunchType } from "@dolphin/types";
 import { css, jsx } from "@emotion/react";
 import Button from "@material-ui/core/Button";
 import CircularProgress from "@material-ui/core/CircularProgress";
-import FormHelperText from "@material-ui/core/FormHelperText";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import { isLinux, isMac } from "common/constants";
-import { shell } from "electron";
+import { remote, shell } from "electron";
 import log from "electron-log";
 import capitalize from "lodash/capitalize";
 import React from "react";
-import { Controller, useForm } from "react-hook-form";
 import { useToasts } from "react-toast-notifications";
 
 import { ConfirmationModal } from "@/components/ConfirmationModal";
@@ -53,11 +51,9 @@ const useStyles = makeStyles((theme: Theme) =>
 export const DolphinSettings: React.FC<{ dolphinType: DolphinLaunchType }> = ({ dolphinType }) => {
   const [dolphinPath, setDolphinPath] = useDolphinPath(dolphinType);
   const [resetModalOpen, setResetModalOpen] = React.useState(false);
-  const [importModalOpen, setImportModalOpen] = React.useState(false);
   const [isResetting, setIsResetting] = React.useState(false);
-  const classes = useStyles();
   const { addToast } = useToasts();
-  const handleError = (err: any) => addToast(err.message ?? JSON.stringify(err), { appearance: "error" });
+  const classes = useStyles();
 
   const openDolphinDirectoryHandler = async () => {
     shell.openItem(dolphinPath);
@@ -82,29 +78,6 @@ export const DolphinSettings: React.FC<{ dolphinType: DolphinLaunchType }> = ({ 
   const clearDolphinCacheHandler = async () => {
     await ipc_clearDolphinCache.renderer!.trigger({ dolphinType });
   };
-
-  const importDolphinHandler = async (importPath: string) => {
-    log.info(`importing dolphin from ${importPath}`);
-
-    await ipc_importDolphinSettings.renderer!.trigger({ toImportDolphinPath: importPath, type: dolphinType });
-    setImportModalOpen(false);
-  };
-
-  const {
-    handleSubmit,
-    watch,
-    control,
-    setValue,
-    formState: { errors },
-  } = useForm<{ importPath: string }>({
-    defaultValues: { importPath: "" },
-  });
-
-  const importPath = watch("importPath", "");
-
-  const onImportFormSubmit = handleSubmit((values) => {
-    importDolphinHandler(values.importPath).catch(handleError);
-  });
 
   return (
     <div>
@@ -159,7 +132,7 @@ export const DolphinSettings: React.FC<{ dolphinType: DolphinLaunchType }> = ({ 
             Clear cache
           </Button>
           <Button variant="outlined" color="secondary" onClick={() => setResetModalOpen(true)} disabled={isResetting}>
-            Reset everything{" "}
+            Reset everything
             {isResetting && (
               <CircularProgress
                 css={css`
@@ -173,48 +146,47 @@ export const DolphinSettings: React.FC<{ dolphinType: DolphinLaunchType }> = ({ 
           </Button>
         </div>
       </SettingItem>
-      {!isLinux && (
-        <SettingItem name={`Import ${dolphinType} Dolphin Settings`}>
-          <ConfirmationModal
-            title="Import Dolphin Settings"
-            open={importModalOpen}
-            onClose={() => setImportModalOpen(false)}
-            onSubmit={onImportFormSubmit}
-            confirmText="Import"
-            closeOnSubmit={false}
-          >
-            Select the location of your old {capitalize(dolphinType)} Dolphin app.
-            <div
-              css={css`
-                margin-top: 20px;
-              `}
-            >
-              <Controller
-                name="importPath"
-                control={control}
-                defaultValue=""
-                render={({ field }) => (
-                  <PathInput
-                    {...field}
-                    value={importPath}
-                    onSelect={(newPath) => setValue("importPath", newPath)}
-                    placeholder={`No ${capitalize(dolphinType)} Dolphin selected`}
-                    options={{
-                      properties: ["openFile"],
-                      filters: [{ name: "Slippi Dolphin", extensions: [isMac ? "app" : "exe"] }],
-                    }}
-                  />
-                )}
-                rules={{ validate: (val) => val.length > 0 || "No path selected" }}
-              />
-              <FormHelperText error={Boolean(errors?.importPath)}>{errors?.importPath?.message}</FormHelperText>
-            </div>
-          </ConfirmationModal>
-          <Button variant="contained" color="secondary" onClick={() => setImportModalOpen(true)} disabled={isResetting}>
-            Import
-          </Button>
-        </SettingItem>
-      )}
+      {!isLinux && <ImportDolphinConfigForm dolphinType={dolphinType} />}
     </div>
+  );
+};
+
+const ImportDolphinConfigForm: React.FC<{
+  dolphinType: DolphinLaunchType;
+}> = ({ dolphinType }) => {
+  const { addToast } = useToasts();
+  const dolphinTypeName = capitalize(dolphinType);
+  const importDolphinHandler = async (importPath: string) => {
+    log.info(`importing dolphin from ${importPath}`);
+    await ipc_importDolphinSettings.renderer!.trigger({ toImportDolphinPath: importPath, type: dolphinType });
+  };
+
+  const onImportClick = async () => {
+    const result = await remote.dialog.showOpenDialog({
+      properties: ["openFile"],
+      filters: [{ name: "Slippi Dolphin", extensions: [isMac ? "app" : "exe"] }],
+    });
+    const res = result.filePaths;
+    if (result.canceled || res.length === 0) {
+      return;
+    }
+    importDolphinHandler(res[0])
+      .then(() => {
+        addToast(`${dolphinTypeName} Dolphin settings successfully imported`, { appearance: "success" });
+      })
+      .catch((err) => {
+        addToast(err.message ?? JSON.stringify(err), { appearance: "error" });
+      });
+  };
+
+  return (
+    <SettingItem
+      name={`Import ${dolphinTypeName} Dolphin Settings`}
+      description={`Replaces the ${dolphinTypeName} Dolphin settings with those from a different Dolphin application.`}
+    >
+      <Button variant="contained" color="secondary" onClick={onImportClick}>
+        Import Dolphin settings
+      </Button>
+    </SettingItem>
   );
 };
