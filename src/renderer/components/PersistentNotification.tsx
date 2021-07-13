021c54fde6f9d0a08d6bfe7038188f62e434876c
Vince Au <vinceau09@gmail.com>
2021-07-13 10:09:59 +1000

    feat: add notification

---
+++ src/renderer/components/PersistentNotification.tsx
@@ -0,0 +1,61 @@
+/** @jsx jsx */
+import { css, jsx } from "@emotion/react";
+import styled from "@emotion/styled";
+import ButtonBase from "@material-ui/core/ButtonBase";
+import { colors } from "common/colors";
+import { ipc_installUpdate } from "common/ipc";
+import React from "react";
+
+import { useAppStore } from "@/lib/hooks/useApp";
+
+export const PersistentNotification: React.FC = () => {
+  const updateVersion = useAppStore((store) => store.updateVersion);
+  const updateReady = useAppStore((store) => store.updateReady);
+
+  const updateHandler = async () => {
+    await ipc_installUpdate.renderer!.trigger({});
+  };
+
+  if (!updateVersion || !updateReady) {
+    return null;
+  }
+
+  return (
+    <Outer>
+      <div
+        css={css`
+          display: flex;
+          justify-content: center;
+        `}
+      >
+        <span
+          css={css`
+            margin-right: 10px;
+          `}
+        >
+          Version {updateVersion} is now available!
+        </span>
+        <RestartButton onClick={updateHandler}>Install update</RestartButton>
+      </div>
+    </Outer>
+  );
+};
+
+const Outer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  position: relative;
+  height: 30px;
+  background-color: ${colors.purpleLight};
+  text-align: center;
+  font-size: 14px;
+`;
+
+const RestartButton = styled(ButtonBase)`
+  font-weight: 500;
+  padding: 0 5px;
+  &:hover {
+    opacity: 0.8;
+  }
+`;
