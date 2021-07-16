a007bb9a8783f41e993550a53622dea1e2191562
Rohan Patel <rspatel014@gmail.com>
2021-07-15 19:07:03 -0700

    moved all gecko code stuff to its own component like <ImportDolphinConfigForm/>

--- src/renderer/containers/Settings/DolphinSettings.tsx
+++ src/renderer/containers/Settings/DolphinSettings.tsx
@@ -67,14 +67,158 @@ const useStyles = makeStyles({
 });
 
 export const DolphinSettings: React.FC<{ dolphinType: DolphinLaunchType }> = ({ dolphinType }) => {
-  const classes = useStyles();
-
   const [dolphinPath, setDolphinPath] = useDolphinPath(dolphinType);
 
   const [resetModalOpen, setResetModalOpen] = React.useState(false);
   const [isResetting, setIsResetting] = React.useState(false);
   const { addToast } = useToasts();
 
+  const openDolphinDirectoryHandler = async () => {
+    shell.openItem(dolphinPath);
+  };
+
+  const configureDolphinHandler = async () => {
+    if (process.platform === "darwin") {
+      addToast("Dolphin may open in the background, please check the app bar", {
+        appearance: "info",
+        autoDismiss: true,
+      });
+    }
+    await ipc_configureDolphin.renderer!.trigger({ dolphinType });
+  };
+
+  const reinstallDolphinHandler = async () => {
+    setIsResetting(true);
+    await ipc_reinstallDolphin.renderer!.trigger({ dolphinType });
+    setIsResetting(false);
+  };
+
+  const clearDolphinCacheHandler = async () => {
+    await ipc_clearDolphinCache.renderer!.trigger({ dolphinType });
+  };
+
+  const dolphinTypeName = capitalize(dolphinType);
+  return (
+    <div>
+      <Typography variant="h5">{dolphinTypeName} Dolphin Settings</Typography>
+      <SettingItem name={`Configure ${dolphinType} Dolphin`}>
+        <div
+          css={css`
+            display: flex;
+            & > button {
+              margin-right: 10px;
+            }
+          `}
+        >
+          <Button variant="contained" color="primary" onClick={configureDolphinHandler} disabled={isResetting}>
+            Configure Dolphin
+          </Button>
+          <Button variant="outlined" color="primary" onClick={openDolphinDirectoryHandler} disabled={isResetting}>
+            Open containing folder
+          </Button>
+        </div>
+      </SettingItem>
+      <DevGuard show={isLinux}>
+        <SettingItem
+          name={`${dolphinType} Dolphin Directory`}
+          description={`The path containing the ${dolphinTypeName} Dolphin executable.`}
+        >
+          <PathInput
+            value={dolphinPath ?? ""}
+            onSelect={setDolphinPath}
+            placeholder="No folder set"
+            options={{ properties: ["openDirectory"] }}
+          />
+        </SettingItem>
+      </DevGuard>
+      {!isLinux && <ImportDolphinConfigForm dolphinType={dolphinType} />}
+      <SettingItem name={`Reset ${dolphinType} Dolphin`}>
+        <ConfirmationModal
+          open={resetModalOpen}
+          onClose={() => setResetModalOpen(false)}
+          onSubmit={reinstallDolphinHandler}
+          title="Are you sure?"
+        >
+          This will remove all your {dolphinType} dolphin settings.
+        </ConfirmationModal>
+        <div
+          css={css`
+            display: flex;
+            & > button {
+              margin-right: 10px;
+            }
+          `}
+        >
+          <Button variant="contained" color="secondary" onClick={clearDolphinCacheHandler} disabled={isResetting}>
+            Clear cache
+          </Button>
+          <Button variant="outlined" color="secondary" onClick={() => setResetModalOpen(true)} disabled={isResetting}>
+            Reset everything
+            {isResetting && (
+              <CircularProgress
+                css={css`
+                  margin-left: 10px;
+                `}
+                size={16}
+                thickness={6}
+                color="inherit"
+              />
+            )}
+          </Button>
+        </div>
+      </SettingItem>
+      <EditGeckoCodes dolphinType={dolphinType} />
+    </div>
+  );
+};
+
+const ImportDolphinConfigForm: React.FC<{
+  dolphinType: DolphinLaunchType;
+}> = ({ dolphinType }) => {
+  const { addToast } = useToasts();
+  const dolphinTypeName = capitalize(dolphinType);
+  const extension = isMac ? "app" : "exe";
+  const importDolphinHandler = async (importPath: string) => {
+    log.info(`importing dolphin from ${importPath}`);
+    await ipc_importDolphinSettings.renderer!.trigger({ toImportDolphinPath: importPath, type: dolphinType });
+  };
+
+  const onImportClick = async () => {
+    const result = await remote.dialog.showOpenDialog({
+      properties: ["openFile"],
+      filters: [{ name: "Slippi Dolphin", extensions: [isMac ? "app" : "exe"] }],
+    });
+    const res = result.filePaths;
+    if (result.canceled || res.length === 0) {
+      return;
+    }
+    importDolphinHandler(res[0])
+      .then(() => {
+        addToast(`${dolphinTypeName} Dolphin settings successfully imported`, { appearance: "success" });
+      })
+      .catch((err) => {
+        addToast(err.message ?? JSON.stringify(err), { appearance: "error" });
+      });
+  };
+
+  return (
+    <SettingItem
+      name={`Import ${dolphinTypeName} Dolphin Settings`}
+      description={`Replace the ${dolphinTypeName} Dolphin settings with those from a different Dolphin application. To do this, select the Dolphin.${extension} with the desired ${dolphinType} settings.`}
+    >
+      <Button variant="contained" color="secondary" onClick={onImportClick}>
+        Import Dolphin settings
+      </Button>
+    </SettingItem>
+  );
+};
+
+const EditGeckoCodes: React.FC<{
+  dolphinType: DolphinLaunchType;
+}> = ({ dolphinType }) => {
+  const { addToast } = useToasts();
+  const classes = useStyles();
+  const [dolphinPath, setDolphinPath] = useDolphinPath(dolphinType);
   //vars for editing gecko codes
   const [tabValue, setTabValue] = React.useState(0);
   const [geckoFormOpen, setGeckoFormOpen] = React.useState(false);
@@ -160,30 +304,6 @@ export const DolphinSettings: React.FC<{ dolphinType: DolphinLaunchType }> = ({ 
     setGeckoCheckboxes(checkboxList);
   }, [geckoCodes]);
 
-  const openDolphinDirectoryHandler = async () => {
-    shell.openItem(dolphinPath);
-  };
-
-  const configureDolphinHandler = async () => {
-    if (process.platform === "darwin") {
-      addToast("Dolphin may open in the background, please check the app bar", {
-        appearance: "info",
-        autoDismiss: true,
-      });
-    }
-    await ipc_configureDolphin.renderer!.trigger({ dolphinType });
-  };
-
-  const reinstallDolphinHandler = async () => {
-    setIsResetting(true);
-    await ipc_reinstallDolphin.renderer!.trigger({ dolphinType });
-    setIsResetting(false);
-  };
-
-  const clearDolphinCacheHandler = async () => {
-    await ipc_clearDolphinCache.renderer!.trigger({ dolphinType });
-  };
-
   const fetchSysInisHandler = async () => {
     return await ipc_fetchSysInis.renderer!.trigger({ dolphinType: dolphinType });
   };
@@ -252,76 +372,8 @@ export const DolphinSettings: React.FC<{ dolphinType: DolphinLaunchType }> = ({ 
     }
   };
 
-  const dolphinTypeName = capitalize(dolphinType);
   return (
     <div>
-      <Typography variant="h5">{dolphinTypeName} Dolphin Settings</Typography>
-      <SettingItem name={`Configure ${dolphinType} Dolphin`}>
-        <div
-          css={css`
-            display: flex;
-            & > button {
-              margin-right: 10px;
-            }
-          `}
-        >
-          <Button variant="contained" color="primary" onClick={configureDolphinHandler} disabled={isResetting}>
-            Configure Dolphin
-          </Button>
-          <Button variant="outlined" color="primary" onClick={openDolphinDirectoryHandler} disabled={isResetting}>
-            Open containing folder
-          </Button>
-        </div>
-      </SettingItem>
-      <DevGuard show={isLinux}>
-        <SettingItem
-          name={`${dolphinType} Dolphin Directory`}
-          description={`The path containing the ${dolphinTypeName} Dolphin executable.`}
-        >
-          <PathInput
-            value={dolphinPath ?? ""}
-            onSelect={setDolphinPath}
-            placeholder="No folder set"
-            options={{ properties: ["openDirectory"] }}
-          />
-        </SettingItem>
-      </DevGuard>
-      {!isLinux && <ImportDolphinConfigForm dolphinType={dolphinType} />}
-      <SettingItem name={`Reset ${dolphinType} Dolphin`}>
-        <ConfirmationModal
-          open={resetModalOpen}
-          onClose={() => setResetModalOpen(false)}
-          onSubmit={reinstallDolphinHandler}
-          title="Are you sure?"
-        >
-          This will remove all your {dolphinType} dolphin settings.
-        </ConfirmationModal>
-        <div
-          css={css`
-            display: flex;
-            & > button {
-              margin-right: 10px;
-            }
-          `}
-        >
-          <Button variant="contained" color="secondary" onClick={clearDolphinCacheHandler} disabled={isResetting}>
-            Clear cache
-          </Button>
-          <Button variant="outlined" color="secondary" onClick={() => setResetModalOpen(true)} disabled={isResetting}>
-            Reset everything
-            {isResetting && (
-              <CircularProgress
-                css={css`
-                  margin-left: 10px;
-                `}
-                size={16}
-                thickness={6}
-                color="inherit"
-              />
-            )}
-          </Button>
-        </div>
-      </SettingItem>
       <SettingItem name={"Gecko Codes"}>
         <Button variant="contained" color="secondary" onClick={() => setGeckoFormOpen(true)}>
           Gecko Codes
@@ -367,46 +419,5 @@ export const DolphinSettings: React.FC<{ dolphinType: DolphinLaunchType }> = ({ 
         </DialogContent>
       </Dialog>
     </div>
-  );
-};
-
-const ImportDolphinConfigForm: React.FC<{
-  dolphinType: DolphinLaunchType;
-}> = ({ dolphinType }) => {
-  const { addToast } = useToasts();
-  const dolphinTypeName = capitalize(dolphinType);
-  const extension = isMac ? "app" : "exe";
-  const importDolphinHandler = async (importPath: string) => {
-    log.info(`importing dolphin from ${importPath}`);
-    await ipc_importDolphinSettings.renderer!.trigger({ toImportDolphinPath: importPath, type: dolphinType });
-  };
-
-  const onImportClick = async () => {
-    const result = await remote.dialog.showOpenDialog({
-      properties: ["openFile"],
-      filters: [{ name: "Slippi Dolphin", extensions: [isMac ? "app" : "exe"] }],
-    });
-    const res = result.filePaths;
-    if (result.canceled || res.length === 0) {
-      return;
-    }
-    importDolphinHandler(res[0])
-      .then(() => {
-        addToast(`${dolphinTypeName} Dolphin settings successfully imported`, { appearance: "success" });
-      })
-      .catch((err) => {
-        addToast(err.message ?? JSON.stringify(err), { appearance: "error" });
-      });
-  };
-
-  return (
-    <SettingItem
-      name={`Import ${dolphinTypeName} Dolphin Settings`}
-      description={`Replace the ${dolphinTypeName} Dolphin settings with those from a different Dolphin application. To do this, select the Dolphin.${extension} with the desired ${dolphinType} settings.`}
-    >
-      <Button variant="contained" color="secondary" onClick={onImportClick}>
-        Import Dolphin settings
-      </Button>
-    </SettingItem>
   );
 };
