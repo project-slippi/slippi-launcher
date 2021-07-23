a291124b1555932010383a647076777d494a61d2
Vince Au <vinceau09@gmail.com>
2021-07-23 12:22:02 +1000

    feat: attempt subscribe to broadcast events

--- src/broadcast/workerInterfaces.ts
+++ src/broadcast/workerInterfaces.ts
@@ -1,14 +1,30 @@
 import { app } from "electron";
-import log from "electron-log";
+import electronLog from "electron-log";
 import { spawn, Thread, Worker } from "threads";
 
 import { Methods as WorkerMethods, WorkerSpec } from "./broadcastWorker";
+import { ipc_broadcastErrorOccurredEvent, ipc_dolphinStatusChangedEvent, ipc_slippiStatusChangedEvent } from "./ipc";
+
+const log = electronLog.scope("broadcast/workerInterface");
 
 export const broadcastWorker: Promise<Thread & WorkerMethods> = new Promise((resolve, reject) => {
   log.debug("broadcast: Spawning worker");
 
   spawn<WorkerSpec>(new Worker("./broadcastWorker"), { timeout: 30000 })
     .then((worker) => {
+      worker.getDolphinStatusObservable().subscribe((status) => {
+        log.info(`got dolphin status: ${status}`);
+        ipc_dolphinStatusChangedEvent.main!.trigger({ status }).catch(log.error);
+      });
+      worker.getSlippiStatusObservable().subscribe((status) => {
+        log.info(`got slippi status: ${status}`);
+        ipc_slippiStatusChangedEvent.main!.trigger({ status }).catch(log.error);
+      });
+      worker.getErrorObservable().subscribe((errorMessage) => {
+        log.info(`got error message: ${errorMessage}`);
+        ipc_broadcastErrorOccurredEvent.main!.trigger({ errorMessage }).catch(log.error);
+      });
+
       log.debug("broadcast: Spawning worker: Done");
 
       async function terminateWorker() {
