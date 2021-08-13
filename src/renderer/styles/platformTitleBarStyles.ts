e04630be240c0b3fddc5ee29a5a79e2477c25d94
Ryan McGrath <ryan@rymc.io>
2021-08-13 14:12:23 -0700

    Swap process check to isMac

--- src/renderer/styles/platformTitleBarStyles.ts
+++ src/renderer/styles/platformTitleBarStyles.ts
@@ -1,12 +1,13 @@
 import { css } from "@emotion/react";
 
+import { isMac } from "../../common/constants";
+
 // Some platforms, like macOS, need to account for extra padding at the top of the window for
 // the proper OS styling.
-export const platformTitleBarStyles =
-  process.platform === "darwin"
-    ? css`
-        padding-top: 32px;
-        -webkit-user-select: none;
-        -webkit-app-region: drag;
-      `
-    : "";
+export const platformTitleBarStyles = isMac
+  ? css`
+      padding-top: 32px;
+      -webkit-user-select: none;
+      -webkit-app-region: drag;
+    `
+  : "";
