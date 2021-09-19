706e78228495dbbb7ee8a5b6d0d290cb8b4f1fcf
Nikhil Narayana <nikhil.narayana@live.com>
2021-09-18 20:23:23 -0700

    fix: handle OBS connection failure gracefully

--- src/console/autoSwitcher.ts
+++ src/console/autoSwitcher.ts
@@ -51,17 +51,21 @@ export class AutoSwitcher extends EventEmitter {
   public async connect() {
     if (this.obsIP && this.obsSourceName) {
       // if you send a password when authentication is disabled, OBS will still connect
-      await this.obs.connect(
-        {
+      try {
+        await this.obs.connect({
           address: this.obsIP,
           password: this.obsPassword,
-        },
-        (err) => {
-          if (err) {
-            this.emit(MirrorEvent.ERROR, err);
-          }
-        },
-      );
+        });
+      } catch (err) {
+        if (err) {
+          this.emit(
+            MirrorEvent.ERROR,
+            "Could not connect to OBS, ensure you have OBS websocket installed and OBS is open.",
+          );
+          return;
+        }
+      }
+
       this.obs.on("SceneItemAdded", async () => this._getSceneSources());
       this.obs.on("SceneItemRemoved", async () => this._getSceneSources());
       await this._getSceneSources();
