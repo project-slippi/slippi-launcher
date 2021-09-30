a689f9adcdb54462c1ac9cebb92b69f9fc0806bc
Vince Au <vinceau09@gmail.com>
2021-09-30 18:41:44 +1000

    feat: allow passing tooltip to checkbox component

--- src/renderer/components/FormInputs/Checkbox.tsx
+++ src/renderer/components/FormInputs/Checkbox.tsx
@@ -1,26 +1,35 @@
 /** @jsx jsx */
 import { css, jsx } from "@emotion/react";
-import MatCheckbox from "@material-ui/core/Checkbox";
+import MatCheckbox, { CheckboxProps as MatCheckboxProps } from "@material-ui/core/Checkbox";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Tooltip from "@material-ui/core/Tooltip";
 
-export interface CheckboxProps {
-  label: string;
-  checked: boolean;
-  onChange: () => void;
+export interface CheckboxProps extends MatCheckboxProps {
+  label: React.ReactNode;
   className?: string;
+  hoverText?: string;
 }
 
-export const Checkbox: React.FC<CheckboxProps> = ({ checked, onChange, label, className }) => (
-  <FormControlLabel
-    className={className}
-    control={<MatCheckbox size="small" checked={checked} onChange={onChange} />}
-    label={label}
-    css={css`
-      margin: 0;
-      .MuiCheckbox-root {
-        padding: 0;
-        padding-right: 5px;
+export const Checkbox: React.FC<CheckboxProps> = (props) => {
+  const { label, className, disabled, hoverText, ...rest } = props;
+  return (
+    <FormControlLabel
+      className={className}
+      control={
+        <Tooltip title={hoverText ?? ""} style={{ cursor: disabled ? "initial" : "pointer" }}>
+          <div>
+            <MatCheckbox size="small" disabled={disabled} {...rest} />
+          </div>
+        </Tooltip>
       }
-    `}
-  />
-);
+      label={label}
+      css={css`
+        margin: 0;
+        .MuiCheckbox-root {
+          padding: 0;
+          padding-right: 5px;
+        }
+      `}
+    />
+  );
+};
