28dd4d0c973a69e846b08fdd89e318b391c9b1a2
Vince Au <vinceau09@gmail.com>
2022-02-28 22:09:32 +1100

    fix: tsconfig paths in jest

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
+const path = require("path");
+const { pathsToModuleNameMapper } = require("ts-jest");
+const { compilerOptions } = require("./tsconfig.json");
+
 module.exports = {
   roots: ["<rootDir>/src"],
   testTimeout: 300000, // 5 minutes in milliseconds
@@ -17,6 +21,7 @@ module.exports = {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/.erb/mocks/fileMock.js",
     "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+    ...pathsToModuleNameMapper(compilerOptions.paths, { prefix: path.join("<rootDir>/", compilerOptions.baseUrl) }),
   },
   moduleFileExtensions: ["js", "jsx", "ts", "tsx", "json"],
   moduleDirectories: ["node_modules", "release/app/node_modules"],
