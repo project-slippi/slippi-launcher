91c52bc7fd3ae73f62b19f3d608ba01b499303b2
Vince Au <vinceau09@gmail.com>
2022-03-05 14:19:55 +1100

    hide .env warning when running yarn

--- .erb/configs/webpack.config.renderer.dev.ts
+++ .erb/configs/webpack.config.renderer.dev.ts
@@ -11,9 +11,6 @@ import checkNodeEnv from "../scripts/check-node-env";
 import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
 import NodePolyfillPlugin from "node-polyfill-webpack-plugin";
 
-// We need to import dotenv here in order to for env variables to be passed into our development main process.
-import "dotenv/config";
-
 // When an ESLint server is running, we can't set the NODE_ENV so we'll check if it's
 // at the dev webpack config is not accidentally run in a production environment
 if (process.env.NODE_ENV === "production") {
