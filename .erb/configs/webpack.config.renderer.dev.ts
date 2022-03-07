20a6f3b0d2d47bb097e38a4ff8c6b7cf394d36ab
Vince Au <vinceau09@gmail.com>
2022-03-07 20:54:02 +1100

    feat: add conditional mock service loading using webpack env

--- .erb/configs/webpack.config.renderer.dev.ts
+++ .erb/configs/webpack.config.renderer.dev.ts
@@ -33,81 +33,94 @@ if (!requiredByDLLConfig && !(fs.existsSync(webpackPaths.dllPath) && fs.existsSy
   execSync("yarn run postinstall");
 }
 
-const configuration: webpack.Configuration = {
-  devtool: "inline-source-map",
+export default (env) => {
+  let useMockServices = false;
+  const { mode } = env;
+  if (mode) {
+    if (mode !== "prod" && mode !== "mock") {
+      throw new Error(`Invalid environment mode ${mode}. Expected 'prod' or 'mock'.`);
+    }
+    if (mode === "mock") {
+      useMockServices = true;
+      console.log(chalk.yellow.bgBlack.bold('Using mock services.'));
+    }
+  }
 
-  mode: "development",
+  const configuration: webpack.Configuration = {
+    devtool: "inline-source-map",
 
-  target: ["web", "electron-renderer"],
+    mode: "development",
 
-  entry: [
-    `webpack-dev-server/client?http://localhost:${port}/dist`,
-    "webpack/hot/only-dev-server",
-    path.join(webpackPaths.srcRendererPath, "index.tsx"),
-  ],
+    target: ["web", "electron-renderer"],
 
-  output: {
-    path: webpackPaths.distRendererPath,
-    publicPath: "/",
-    filename: "renderer.dev.js",
-    library: {
-      type: "umd",
-    },
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.s?css$/,
-        use: [
-          "style-loader",
-          {
-            loader: "css-loader",
-            options: {
-              modules: true,
-              sourceMap: true,
-              importLoaders: 1,
-            },
-          },
-          "sass-loader",
-        ],
-        include: /\.module\.s?(c|a)ss$/,
-      },
-      {
-        test: /\.s?css$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-        exclude: /\.module\.s?(c|a)ss$/,
-      },
-      // Fonts
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
-      },
-      // Allow direct importing of SVGs
-      {
-        test: /\.svg$/,
-        use: ["@svgr/webpack", "url-loader"],
-      },
-      // Images
-      {
-        test: /\.(png|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
+    entry: [
+      `webpack-dev-server/client?http://localhost:${port}/dist`,
+      "webpack/hot/only-dev-server",
+      path.join(webpackPaths.srcRendererPath, "index.tsx"),
     ],
-  },
 
-  resolve: {
-    fallback: {
-      net: false,
-      dgram: false,
-      fs: false,
+    output: {
+      path: webpackPaths.distRendererPath,
+      publicPath: "/",
+      filename: "renderer.dev.js",
+      library: {
+        type: "umd",
+      },
     },
-  },
 
-  plugins: [
-    ...(requiredByDLLConfig
-      ? []
-      : [
+    module: {
+      rules: [
+        {
+          test: /\.s?css$/,
+          use: [
+            "style-loader",
+            {
+              loader: "css-loader",
+              options: {
+                modules: true,
+                sourceMap: true,
+                importLoaders: 1,
+              },
+            },
+            "sass-loader",
+          ],
+          include: /\.module\.s?(c|a)ss$/,
+        },
+        {
+          test: /\.s?css$/,
+          use: ["style-loader", "css-loader", "sass-loader"],
+          exclude: /\.module\.s?(c|a)ss$/,
+        },
+        // Fonts
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: "asset/resource",
+        },
+        // Allow direct importing of SVGs
+        {
+          test: /\.svg$/,
+          use: ["@svgr/webpack", "url-loader"],
+        },
+        // Images
+        {
+          test: /\.(png|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
+        },
+      ],
+    },
+
+    resolve: {
+      fallback: {
+        net: false,
+        dgram: false,
+        fs: false,
+      },
+    },
+
+    plugins: [
+      ...(requiredByDLLConfig
+        ? []
+        : [
           new webpack.DllReferencePlugin({
             context: webpackPaths.dllPath,
             manifest: require(manifest),
@@ -115,77 +128,81 @@ const configuration: webpack.Configuration = {
           }),
         ]),
 
-    new webpack.NoEmitOnErrorsPlugin(),
-    new NodePolyfillPlugin(),
-    new webpack.NormalModuleReplacementPlugin(/(.*)installServices(\.*)/, function (resource) {
-      resource.request = resource.request.replace(/installServices/, `installMockServices`);
-    }),
+      new webpack.NoEmitOnErrorsPlugin(),
+      new NodePolyfillPlugin(),
 
-    /**
-     * Create global constants which can be configured at compile time.
-     *
-     * Useful for allowing different behaviour between development builds and
-     * release builds
-     *
-     * NODE_ENV should be production so that modules do not perform certain
-     * development checks
-     *
-     * By default, use 'development' as NODE_ENV. This can be overriden with
-     * 'staging', for example, by changing the ENV variables in the npm scripts
-     */
-    new webpack.EnvironmentPlugin({
-      NODE_ENV: "development",
-    }),
+      ...(useMockServices ?
+        [new webpack.NormalModuleReplacementPlugin(/(.*)installServices(\.*)/, function (resource) {
+          resource.request = resource.request.replace(/installServices/, `installMockServices`);
+        })]
+        : []),
 
-    new webpack.LoaderOptionsPlugin({
-      debug: true,
-    }),
+      /**
+       * Create global constants which can be configured at compile time.
+       *
+       * Useful for allowing different behaviour between development builds and
+       * release builds
+       *
+       * NODE_ENV should be production so that modules do not perform certain
+       * development checks
+       *
+       * By default, use 'development' as NODE_ENV. This can be overriden with
+       * 'staging', for example, by changing the ENV variables in the npm scripts
+       */
+      new webpack.EnvironmentPlugin({
+        NODE_ENV: "development",
+      }),
 
-    new ReactRefreshWebpackPlugin(),
+      new webpack.LoaderOptionsPlugin({
+        debug: true,
+      }),
 
-    new HtmlWebpackPlugin({
-      filename: path.join("index.html"),
-      template: path.join(webpackPaths.srcRendererPath, "index.ejs"),
-      minify: {
-        collapseWhitespace: true,
-        removeAttributeQuotes: true,
-        removeComments: true,
+      new ReactRefreshWebpackPlugin(),
+
+      new HtmlWebpackPlugin({
+        filename: path.join("index.html"),
+        template: path.join(webpackPaths.srcRendererPath, "index.ejs"),
+        minify: {
+          collapseWhitespace: true,
+          removeAttributeQuotes: true,
+          removeComments: true,
+        },
+        isBrowser: false,
+        env: process.env.NODE_ENV,
+        isDevelopment: process.env.NODE_ENV !== "production",
+        nodeModules: webpackPaths.appNodeModulesPath,
+      }),
+    ],
+
+    node: {
+      __dirname: false,
+      __filename: false,
+    },
+
+    // @ts-ignore
+    devServer: {
+      port,
+      compress: true,
+      hot: true,
+      headers: { "Access-Control-Allow-Origin": "*" },
+      static: {
+        publicPath: "/",
       },
-      isBrowser: false,
-      env: process.env.NODE_ENV,
-      isDevelopment: process.env.NODE_ENV !== "production",
-      nodeModules: webpackPaths.appNodeModulesPath,
-    }),
-  ],
-
-  node: {
-    __dirname: false,
-    __filename: false,
-  },
-
-  // @ts-ignore
-  devServer: {
-    port,
-    compress: true,
-    hot: true,
-    headers: { "Access-Control-Allow-Origin": "*" },
-    static: {
-      publicPath: "/",
+      historyApiFallback: {
+        verbose: true,
+      },
+      onBeforeSetupMiddleware() {
+        console.log("Starting Main Process...");
+        spawn("yarn", ["run", "start:main"], {
+          shell: true,
+          env: process.env,
+          stdio: "inherit",
+        })
+          .on("close", (code: number) => process.exit(code!))
+          .on("error", (spawnError) => console.error(spawnError));
+      },
     },
-    historyApiFallback: {
-      verbose: true,
-    },
-    onBeforeSetupMiddleware() {
-      console.log("Starting Main Process...");
-      spawn("yarn", ["run", "start:main"], {
-        shell: true,
-        env: process.env,
-        stdio: "inherit",
-      })
-        .on("close", (code: number) => process.exit(code!))
-        .on("error", (spawnError) => console.error(spawnError));
-    },
-  },
-};
+  };
 
-export default merge(baseConfig, configuration);
+  return merge(baseConfig, configuration);
+}
