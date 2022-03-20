a488ab51fa5f9f0229372852ae590645e49959c3
Vince Au <vinceau09@gmail.com>
2022-03-20 22:55:41 +1100

    feat: upgrade theme to v5

--- src/renderer/styles/theme.ts
+++ src/renderer/styles/theme.ts
@@ -1,79 +1,92 @@
 import { colors } from "@common/colors";
 import type { Theme } from "@mui/material/styles";
-import { adaptV4Theme, createTheme } from "@mui/material/styles";
+import { createTheme } from "@mui/material/styles";
 
 import { withFont } from "./withFont";
 
 const rubikFont = withFont("Rubik");
 const mavenProFont = withFont("Maven Pro");
 
-const theme = createTheme(
-  adaptV4Theme({
-    palette: {
-      mode: "dark",
-      text: {
-        primary: "#E9EAEA",
-        secondary: "#B4B4B4",
-      },
-      primary: {
-        main: colors.greenPrimary,
-      },
-      secondary: {
-        main: colors.purplePrimary,
-      },
-      divider: "rgba(255,255,255)",
-      background: {
-        paper: colors.purpleDark,
-        default: colors.purple,
-      },
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+    text: {
+      primary: "#E9EAEA",
+      secondary: "#B4B4B4",
     },
-    typography: {
-      fontFamily: rubikFont,
-      fontSize: 16,
-      h1: {
-        fontFamily: mavenProFont,
-      },
-      h2: {
-        fontFamily: mavenProFont,
-      },
-      h3: {
-        fontFamily: mavenProFont,
-      },
-      h4: {
-        fontFamily: mavenProFont,
-      },
-      h5: {
-        fontFamily: mavenProFont,
-      },
-      h6: {
-        fontFamily: mavenProFont,
-      },
-      caption: {
-        opacity: 0.6,
-      },
+    primary: {
+      main: colors.greenPrimary,
     },
-  }),
-);
+    secondary: {
+      main: colors.purplePrimary,
+    },
+    divider: "rgba(255,255,255)",
+    background: {
+      paper: colors.purpleDark,
+      default: colors.purple,
+    },
+  },
+  typography: {
+    fontFamily: rubikFont,
+    fontSize: 16,
+    h1: {
+      fontFamily: mavenProFont,
+    },
+    h2: {
+      fontFamily: mavenProFont,
+    },
+    h3: {
+      fontFamily: mavenProFont,
+    },
+    h4: {
+      fontFamily: mavenProFont,
+    },
+    h5: {
+      fontFamily: mavenProFont,
+    },
+    h6: {
+      fontFamily: mavenProFont,
+    },
+    caption: {
+      opacity: 0.6,
+    },
+  },
+});
 
 const addOverrides = (theme: Theme) => {
-  return createTheme(
-    adaptV4Theme({
-      ...theme,
-      props: {
-        MuiTooltip: {
+  return createTheme({
+    ...theme,
+    components: {
+      MuiTooltip: {
+        defaultProps: {
           arrow: true,
         },
-        MuiTextField: {
+        styleOverrides: {
+          arrow: {
+            color: colors.offWhite,
+          },
+          tooltip: {
+            backgroundColor: colors.offWhite,
+            color: colors.purpleDarker,
+            boxShadow: theme.shadows[1],
+            fontSize: 13,
+          },
+        },
+      },
+      MuiTextField: {
+        defaultProps: {
           variant: "filled",
           fullWidth: true,
           size: "small",
         },
-        MuiRadio: {
+      },
+      MuiRadio: {
+        defaultProps: {
           color: "primary",
         },
       },
-      overrides: {
-        MuiPaper: {
+      MuiPaper: {
+        styleOverrides: {
           root: {
             borderStyle: "solid",
             borderWidth: "1px",
@@ -85,12 +98,16 @@ const addOverrides = (theme: Theme) => {
             overflow: "hidden",
           },
         },
-        MuiTableCell: {
+      },
+      MuiTableCell: {
+        styleOverrides: {
           root: {
             borderBottomColor: "#1E1F25",
           },
         },
-        MuiInputLabel: {
+      },
+      MuiInputLabel: {
+        styleOverrides: {
           root: {
             color: "#dddddd",
             "&.Mui-focused": {
@@ -98,12 +115,16 @@ const addOverrides = (theme: Theme) => {
             },
           },
         },
-        MuiListItemIcon: {
+      },
+      MuiListItemIcon: {
+        styleOverrides: {
           root: {
             minWidth: "initial",
           },
         },
-        MuiMenuItem: {
+      },
+      MuiMenuItem: {
+        styleOverrides: {
           root: {
             "&.Mui-selected": {
               backgroundColor: "rgba(255, 255, 255, 0.16)",
@@ -113,7 +134,9 @@ const addOverrides = (theme: Theme) => {
             },
           },
         },
-        MuiListItem: {
+      },
+      MuiListItem: {
+        styleOverrides: {
           root: {
             "&.Mui-selected": {
               backgroundColor: "rgba(255, 255, 255, 0.16)",
@@ -123,23 +146,16 @@ const addOverrides = (theme: Theme) => {
             },
           },
         },
-        MuiTooltip: {
-          arrow: {
-            color: colors.offWhite,
-          },
-          tooltip: {
-            backgroundColor: colors.offWhite,
-            color: colors.purpleDarker,
-            boxShadow: theme.shadows[1],
-            fontSize: 13,
-          },
-        },
-        MuiButtonBase: {
+      },
+      MuiButtonBase: {
+        styleOverrides: {
           root: {
             fontFamily: rubikFont,
           },
         },
-        MuiButton: {
+      },
+      MuiButton: {
+        styleOverrides: {
           root: {
             borderRadius: "10px",
           },
@@ -154,8 +170,8 @@ const addOverrides = (theme: Theme) => {
           },
         },
       },
-    }),
-  );
+    },
+  });
 };
 
 export const slippiTheme = addOverrides(theme);
