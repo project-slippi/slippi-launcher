058c2ae3a9acc429a86d99030c62c3cd2422207c
Vince Au <vinceau09@gmail.com>
2022-03-26 13:37:55 +1100

    feat: add storybook and related configs

---
+++ .storybook/manager.js
@@ -0,0 +1,23 @@
+const { addons } = require('@storybook/addons');
+
+addons.setConfig({
+	isFullscreen: false,
+	showNav: true,
+	showPanel: true,
+	panelPosition: 'bottom',
+	enableShortcuts: true,
+	isToolshown: true,
+	selectedPanel: undefined,
+	initialActive: 'sidebar',
+	sidebar: {
+		showRoots: false,
+		collapsedRoots: ['other'],
+	},
+	toolbar: {
+		title: { hidden: false, },
+		zoom: { hidden: false, },
+		eject: { hidden: false, },
+		copy: { hidden: false, },
+		fullscreen: { hidden: false, },
+	},
+});
---
+++ .storybook/preview.js
@@ -0,0 +1,11 @@
+import "../src/renderer/styles/styles.scss"
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
