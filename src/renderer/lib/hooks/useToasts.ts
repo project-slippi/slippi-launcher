4f7e7d250ad29c76f7e19becc4dfa86ac7299794
Vince Au <vinceau09@gmail.com>
2022-03-27 18:41:43 +1100

    fix: useToast causing infinite re-renders

--- src/renderer/lib/hooks/useToasts.ts
+++ src/renderer/lib/hooks/useToasts.ts
@@ -1,10 +1,15 @@
 import { useSnackbar } from "notistack";
+import React from "react";
 
 export const useToasts = () => {
   const { enqueueSnackbar } = useSnackbar();
-  return {
-    showError: (message: string) => enqueueSnackbar(message, { variant: "error" }),
-    showSuccess: (message: string) => enqueueSnackbar(message, { variant: "success" }),
-    showWarning: (message: string) => enqueueSnackbar(message, { variant: "warning" }),
-  };
+  const toastHandlers = React.useMemo(
+    () => ({
+      showError: (message: string) => enqueueSnackbar(message, { variant: "error" }),
+      showSuccess: (message: string) => enqueueSnackbar(message, { variant: "success" }),
+      showWarning: (message: string) => enqueueSnackbar(message, { variant: "warning" }),
+    }),
+    [enqueueSnackbar],
+  );
+  return toastHandlers;
 };
