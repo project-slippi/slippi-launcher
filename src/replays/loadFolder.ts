e4920d28c87ecd301e9517d71f90dede44a8a081
Vince Au <vinceau09@gmail.com>
2022-06-25 23:30:45 +1000

    refactor: clean up load replays function

--- src/replays/loadFolder.ts
+++ src/replays/loadFolder.ts
@@ -8,27 +8,21 @@ export async function loadReplays(
   callback?: (current: number, total: number) => void,
 ): Promise<FileLoadResult> {
   let count = 0;
-  const total = filesToLoad.length;
 
   const process = async (path: string) => {
     return new Promise<FileResult | null>((resolve) => {
-      setImmediate(async () => {
-        try {
-          resolve(await loadFile(path));
-        } catch (err) {
-          resolve(null);
-        } finally {
-          callback && callback(count++, total);
-        }
+      setImmediate(() => {
+        loadFile(path)
+          .then(resolve)
+          .catch(() => resolve(null))
+          .finally(() => {
+            callback && callback(count++, filesToLoad.length);
+          });
       });
     });
   };
 
-  const slpGames = await Promise.all(
-    filesToLoad.map((fullPath) => {
-      return process(fullPath);
-    }),
-  );
+  const slpGames = await Promise.all(filesToLoad.map((fullPath) => process(fullPath)));
 
   return {
     files: slpGames.filter(exists),
