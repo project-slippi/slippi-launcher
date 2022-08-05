417b081eb7836d5f7ad497c5e1fb3e7d299ad6ad
Vince Au <vinceau09@gmail.com>
2022-08-05 23:11:45 +1000

    fix: always overwrite existing file when updating dolphin
    Hopefully this can assist in fixing dolphin installation issues.

--- src/dolphin/install/download.ts
+++ src/dolphin/install/download.ts
@@ -1,7 +1,6 @@
 import log from "electron-log";
 import path from "path";
 import { download } from "utils/download";
-import { fileExists } from "utils/fileExists";
 
 export async function downloadLatestDolphin(
   downloadUrl: string,
@@ -11,18 +10,13 @@ export async function downloadLatestDolphin(
   const parsedUrl = new URL(downloadUrl);
   const filename = path.basename(parsedUrl.pathname);
   const downloadLocation = path.join(destinationFolder, filename);
-  const exists = await fileExists(downloadLocation);
-  if (!exists) {
-    log.info(`Downloading ${downloadUrl} to ${downloadLocation}`);
-    await download({
-      url: downloadUrl,
-      destinationFile: downloadLocation,
-      overwrite: true,
-      onProgress: ({ transferredBytes, totalBytes }) => onProgress && onProgress(transferredBytes, totalBytes),
-    });
-    log.info(`Successfully downloaded ${downloadUrl} to ${downloadLocation}`);
-  } else {
-    log.info(`${downloadLocation} already exists. Skipping download.`);
-  }
+  log.info(`Downloading ${downloadUrl} to ${downloadLocation}`);
+  await download({
+    url: downloadUrl,
+    destinationFile: downloadLocation,
+    overwrite: true,
+    onProgress: ({ transferredBytes, totalBytes }) => onProgress && onProgress(transferredBytes, totalBytes),
+  });
+  log.info(`Successfully downloaded ${downloadUrl} to ${downloadLocation}`);
   return downloadLocation;
 }
