c61788eb845ee68ac644e7a4c10cbf5e020f005d
Nikhil Narayana <nikhil.narayana@live.com>
2022-12-26 17:16:51 -0800

    add application support folder menu item for macOS

--- src/main/main.ts
+++ src/main/main.ts
@@ -125,6 +125,10 @@ const createWindow = async () => {
     },
     onOpenReplayFile: playReplayAndShowStats,
     createWindow,
+    onOpenAppSupportFolder: () => {
+      const path = app.getPath("userData");
+      void shell.openPath(path);
+    },
     enableDevTools: isDevelopment,
   });
   menu = menuBuilder.buildMenu();
