f9c4c8b0f9ceb34ce60340ffb93384e24eef4bf4
Vince Au <vinceau09@gmail.com>
2023-08-24 12:30:03 +1000

    fix esm bs

---
+++ .erb/scripts/fix-esm.js
@@ -0,0 +1,4 @@
+// Use the global hook of fix-esm to fix ESM imports
+// https://www.npmjs.com/package/fix-esm?activeTab=readme
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require("fix-esm").register();
