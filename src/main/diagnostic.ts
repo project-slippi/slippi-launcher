d72b7e4f59daad87f026058be5fdb6bc0ef5e9e6
jmlee337 <jmlee337@gmail.com>
2023-09-02 11:57:01 +0900

    implement NAT type diagnostic

--- src/main/diagnostic.ts
+++ src/main/diagnostic.ts
@@ -1,8 +1,15 @@
 import type { PortMapping } from "@common/types";
 import { CgnatPresence, NatpmpPresence, NatType, UpnpPresence } from "@common/types";
+import { createServer, request } from "stun";
 
 export async function natType(): Promise<{ address: string; natType: NatType }> {
-  return { address: "127.0.0.1", natType: NatType.NORMAL };
+  const stunServer = createServer({ type: "udp4" });
+  const stunResponse1 = await request("stun1.l.google.com:19302", { server: stunServer });
+  const stunResponse2 = await request("stun2.l.google.com:19302", { server: stunServer });
+  const address1 = stunResponse1.getXorAddress();
+  const address2 = stunResponse2.getXorAddress();
+  stunServer.close();
+  return { address: address1.address, natType: address1.port === address2.port ? NatType.NORMAL : NatType.SYMMETRIC };
 }
 
 export async function portMapping(): Promise<PortMapping> {
