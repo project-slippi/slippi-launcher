ffb7c954d9364b11ebc27e962449fbf4c326e3b1
jmlee337 <jmlee337@gmail.com>
2023-09-02 16:58:00 +0900

    implement port mapping diagnostic

--- src/main/diagnostic.ts
+++ src/main/diagnostic.ts
@@ -1,5 +1,7 @@
 import type { PortMapping } from "@common/types";
 import { CgnatPresence, NatpmpPresence, NatType, UpnpPresence } from "@common/types";
+import { createPmpClient, createUpnpClient } from "@xmcl/nat-api";
+import { gateway4async } from "default-gateway";
 import { createServer, request } from "stun";
 
 export async function natType(): Promise<{ address: string; natType: NatType }> {
@@ -13,7 +15,47 @@ export async function natType(): Promise<{ address: string; natType: NatType }> 
 }
 
 export async function portMapping(): Promise<PortMapping> {
-  return { upnp: UpnpPresence.ABSENT, natpmp: NatpmpPresence.ABSENT } as PortMapping;
+  let upnpPresence = UpnpPresence.UNKNOWN;
+  const upnpClient = await createUpnpClient();
+  const upnpPromise = upnpClient
+    .externalIp()
+    .then(() => {
+      upnpPresence = UpnpPresence.PRESENT;
+    })
+    .catch(() => {
+      upnpPresence = UpnpPresence.ABSENT;
+    })
+    .finally(() => {
+      upnpClient.destroy();
+    });
+
+  let natpmpPresence = NatpmpPresence.UNKNOWN;
+  const pmpClient = await createPmpClient((await gateway4async()).gateway);
+  const pmpPromise = new Promise((resolve, reject) => {
+    // library does not use a timeout for NAT-PMP, so we do it ourselves.
+    const timeout = setTimeout(() => {
+      reject("NAT-PMP timeout");
+    }, 1800); // same as library UPnP timeout
+    pmpClient
+      .externalIp()
+      .then(resolve)
+      .catch(reject)
+      .finally(() => {
+        clearTimeout(timeout);
+      });
+  })
+    .then(() => {
+      natpmpPresence = NatpmpPresence.PRESENT;
+    })
+    .catch(() => {
+      natpmpPresence = NatpmpPresence.ABSENT;
+    })
+    .finally(() => {
+      pmpClient.close();
+    });
+
+  await Promise.all([upnpPromise, pmpPromise]);
+  return { upnp: upnpPresence, natpmp: natpmpPresence } as PortMapping;
 }
 
 export async function cgnat(address: string): Promise<{ cgnat: CgnatPresence }> {
