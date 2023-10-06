37ede69dcad54a9b789d536210be44db1415d6ee
Nikhil Narayana <nikhil.narayana@live.com>
2023-10-05 20:00:16 -0700

    chore: mention `include crash context` in some dolphin error msgs

--- src/renderer/lib/dolphin/handleDolphinExitCode.ts
+++ src/renderer/lib/dolphin/handleDolphinExitCode.ts
@@ -29,14 +29,14 @@ const handleWindowsExitCode = (exitCode: number): string | null => {
       return "Required DLLs for launching Dolphin are missing. Check the Help section in the settings page to fix this issue.";
     }
     case 0xc0000409: {
-      return 'Dolphin has crashed. Please go to the Help section of the settings page, click "Copy logs", and paste them in the Slippi Discord\'s #windows-support channel.';
+      return 'Dolphin has crashed. Please go to the Help section of the settings page, click "Copy logs", and paste them in the Slippi Discord\'s #windows-support channel with some context regarding the crash.';
     }
     case 0xc0000005: {
       return "Try a different video backend in Dolphin. If the issue persists, install the latest Windows update available and then restart your computer.";
     }
     default: {
       return `Dolphin exited with error code: 0x${exitCode.toString(16)}.
-      If you're in need of assistance, screenshot this and post it in a support channel in the Slippi Discord.`;
+      If you're in need of assistance, screenshot this and post it in a support channel in the Slippi Discord with some context regarding the crash.`;
     }
   }
 };
@@ -44,11 +44,11 @@ const handleWindowsExitCode = (exitCode: number): string | null => {
 const handleLinuxExitCode = (exitCode: number): string => {
   switch (exitCode) {
     case 0x7f: {
-      return "Required libraries for launching Dolphin may be missing. Check the Help section in the settings page for guidance. Post in the Slippi Discord's linux-support channel for further assistance if needed.";
+      return "Required libraries for launching Dolphin may be missing. Check the Help section in the settings page for guidance. Post in the Slippi Discord's linux-support forum for further assistance if needed.";
     }
     default: {
       return `Dolphin exited with error code: 0x${exitCode.toString(16)}.
-      If you're in need of assistance, screenshot this and post it in a support channel in the Slippi Discord.`;
+      If you're in need of assistance, screenshot this and post it in a support channel in the Slippi Discord with some context regarding the crash.`;
     }
   }
 };
