5a5d6ec4da9dbd09ab09a466d65f2c6809e2c678
Nikhil Narayana <nikhil.narayana@live.com>
2023-10-06 17:19:46 -0700

    fix up macOS install

--- src/dolphin/install/extractDmg.ts
+++ src/dolphin/install/extractDmg.ts
@@ -21,7 +21,7 @@ export async function extractDmg(
   return files;
 }
 
-async function mountDmg(filename: string): Promise<string> {
+export async function mountDmg(filename: string): Promise<string> {
   return new Promise((resolve, reject) => {
     dmg.mount(filename, (err, value) => {
       if (err) {
@@ -33,7 +33,7 @@ async function mountDmg(filename: string): Promise<string> {
   });
 }
 
-async function unmountDmg(mountPath: string): Promise<void> {
+export async function unmountDmg(mountPath: string): Promise<void> {
   return new Promise((resolve, reject) => {
     dmg.unmount(mountPath, (err) => {
       if (err) {
--- src/dolphin/install/macos.ts
+++ src/dolphin/install/macos.ts
@@ -2,7 +2,7 @@ import * as fs from "fs-extra";
 import os from "os";
 import path from "path";
 
-import { extractDmg } from "./extractDmg";
+import { extractDmg, mountDmg, unmountDmg } from "./extractDmg";
 
 export async function installIshiirukaDolphinOnMac({
   assetPath,
@@ -14,11 +14,15 @@ export async function installIshiirukaDolphinOnMac({
   log?: (message: string) => void;
 }) {
   log(`Extracting to: ${destinationFolder}`);
-  const filter = (src: string, _dest: string) => {
-    console.log(`[extractDmg.filter] src=${src}`);
-    return src.endsWith("/Slippi Dolphin.app") || src.includes("/Slippi Dolphin.app/");
-  };
-  await extractDmg(assetPath, destinationFolder, { dereference: true, filter });
+  await extractDmg(assetPath, destinationFolder);
+  const files = await fs.readdir(destinationFolder);
+  await Promise.all(
+    files
+      .filter((file) => file !== "Slippi Dolphin.app")
+      .map(async (file) => {
+        await fs.remove(path.join(destinationFolder, file));
+      }),
+  );
 
   // sometimes permissions aren't set properly after the extraction so we will forcibly set them on install
   const binaryLocation = path.join(destinationFolder, "Slippi Dolphin.app", "Contents", "MacOS", "Slippi Dolphin");
@@ -39,17 +43,26 @@ export async function installMainlineDolphinOnMac({
   log?: (message: string) => void;
 }) {
   log(`Extracting to: ${destinationFolder}`);
-  const filter = (src: string, _dest: string) => {
-    console.log(`[extractDmg.filter] src=${src}`);
-    return src.endsWith("/Slippi_Dolphin.app") || src.includes("/Slippi_Dolphin.app/");
-  };
-  await extractDmg(assetPath, destinationFolder, { dereference: true, filter });
+  const mountPath = await mountDmg(assetPath);
+  try {
+    const appMountPath = path.join(mountPath, "Slippi_Dolphin.app");
+    const destPath = path.join(destinationFolder, "Slippi_Dolphin.app");
+    await fs.copy(appMountPath, destPath, { recursive: true });
+  } catch {
+    log("Failed to copy files from DMG");
+  } finally {
+    await unmountDmg(mountPath);
+  }
 
-  // sometimes permissions aren't set properly after the extraction so we will forcibly set them on install
-  const binaryLocation = path.join(destinationFolder, "Slippi_Dolphin.app", "Contents", "MacOS", "Slippi_Dolphin");
-  const userInfo = os.userInfo();
-  await fs.chmod(path.join(destinationFolder, "Slippi_Dolphin.app"), "777");
-  await fs.chown(path.join(destinationFolder, "Slippi_Dolphin.app"), userInfo.uid, userInfo.gid);
-  await fs.chmod(binaryLocation, "777");
-  await fs.chown(binaryLocation, userInfo.uid, userInfo.gid);
+  try {
+    // sometimes permissions aren't set properly after the extraction so we will forcibly set them on install
+    const binaryLocation = path.join(destinationFolder, "Slippi_Dolphin.app", "Contents", "MacOS", "Slippi_Dolphin");
+    const userInfo = os.userInfo();
+    await fs.chmod(path.join(destinationFolder, "Slippi_Dolphin.app"), "777");
+    await fs.chown(path.join(destinationFolder, "Slippi_Dolphin.app"), userInfo.uid, userInfo.gid);
+    await fs.chmod(binaryLocation, "777");
+    await fs.chown(binaryLocation, userInfo.uid, userInfo.gid);
+  } catch {
+    log("could not chown/chmod Dolphin");
+  }
 }
