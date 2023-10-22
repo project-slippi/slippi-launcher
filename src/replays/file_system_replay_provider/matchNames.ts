775abf4bb18fa0a6dc19e058fe9caccb22a442f7
Vince Au <vinceau09@gmail.com>
2023-10-22 22:24:17 +1100

    clean up

--- src/replays/file_system_replay_provider/matchNames.ts
+++ src/replays/file_system_replay_provider/matchNames.ts
@@ -1,7 +1,7 @@
 import type { GameStartType, MetadataType } from "@slippi/slippi-js";
 import get from "lodash/get";
 
-export type PlayerNames = {
+type PlayerNames = {
   name: string;
   code: string;
   tag: string;
@@ -24,38 +24,4 @@ export function extractPlayerNames(
   result.code = player?.connectCode || get(metadata, ["players", index, "names", "code"], "");
 
   return result;
-}
-
-export function extractAllPlayerNames(settings: GameStartType, metadata?: MetadataType | null): string[] {
-  const result: string[] = [];
-  for (const player of settings.players) {
-    const names = extractPlayerNames(player.playerIndex, settings, metadata);
-    result.push(...Object.values(names).filter((n) => Boolean(n)));
-  }
-  return result;
-}
-
-export function namesMatch(lookingForNametags: string[], inGameTags: string[], fuzzyMatch = true): boolean {
-  if (lookingForNametags.length === 0 || inGameTags.length === 0) {
-    return false;
-  }
-
-  const match = inGameTags.find((name) => {
-    // If we're not doing fuzzy matching just return the exact match
-    if (!fuzzyMatch) {
-      return lookingForNametags.includes(name);
-    }
-
-    // Replace the netplay names with underscores and coerce to lowercase
-    // Smashladder internally represents spaces as underscores when writing SLP files
-    const fuzzyNetplayName = name.toLowerCase();
-    const matchedFuzzyTag = lookingForNametags.find((tag) => {
-      const lowerSearch = tag.toLowerCase();
-      const fuzzySearch = tag.split(" ").join("_").toLowerCase();
-      return fuzzyNetplayName.startsWith(lowerSearch) || fuzzyNetplayName.startsWith(fuzzySearch);
-    });
-    return matchedFuzzyTag !== undefined;
-  });
-
-  return match !== undefined;
 }
