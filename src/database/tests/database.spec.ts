dfa59a9d80675bb21f34fa33b8bca28763316392
Vince Au <vinceau09@gmail.com>
2023-10-28 13:06:44 +1100

    add database tests

---
+++ src/database/tests/database.spec.ts
@@ -0,0 +1,27 @@
+import { createDatabase } from "database/create_database";
+import type { Database } from "database/schema";
+import type { Kysely } from "kysely";
+
+import * as ReplayRepository from "../repositories/replay_repository";
+
+describe("when validating display names", () => {
+  let db: Kysely<Database>;
+
+  beforeAll(async () => {
+    db = await createDatabase();
+  });
+
+  afterEach(async () => {
+    // Clear the database after each test
+    await db.deleteFrom("replay").execute();
+    await db.deleteFrom("game").execute();
+    await db.deleteFrom("player").execute();
+  });
+
+  it("should disallow replays with the same folder and filename", async () => {
+    await ReplayRepository.insertReplay(db, { folder: "folder", file_name: "name", size_bytes: 10 });
+
+    const result = ReplayRepository.insertReplay(db, { folder: "folder", file_name: "name", size_bytes: 10 });
+    await expect(result).rejects.toThrowError();
+  });
+});
