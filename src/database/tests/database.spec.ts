c48af209f050bf31610699fe18f1aa4db987c5bb
Vince Au <vinceau09@gmail.com>
2023-10-28 13:16:48 +1100

    add test for row count

--- src/database/tests/database.spec.ts
+++ src/database/tests/database.spec.ts
@@ -20,6 +20,14 @@ describe("when validating display names", () => {
 
   it("should disallow replays with the same folder and filename", async () => {
     await ReplayRepository.insertReplay(db, { folder: "folder", file_name: "name", size_bytes: 10 });
+    await ReplayRepository.insertReplay(db, { folder: "folder", file_name: "different_name", size_bytes: 10 });
+    await ReplayRepository.insertReplay(db, { folder: "different_folder", file_name: "name", size_bytes: 10 });
+
+    const { num_rows } = await db
+      .selectFrom("replay")
+      .select((eb) => eb.fn.countAll<number>().as("num_rows"))
+      .executeTakeFirstOrThrow();
+    expect(num_rows).toEqual(3);
 
     const result = ReplayRepository.insertReplay(db, { folder: "folder", file_name: "name", size_bytes: 10 });
     await expect(result).rejects.toThrowError();
