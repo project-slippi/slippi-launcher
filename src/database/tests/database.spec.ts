a358fc72917da8760169b98e51d911cdc06c5a9e
Vince Au <vinceau09@gmail.com>
2023-10-30 21:00:47 +1100

    remove test repository file

--- src/database/tests/database.spec.ts
+++ src/database/tests/database.spec.ts
@@ -8,7 +8,6 @@ import * as GameRepository from "../repositories/game_repository";
 import * as PlayerRepository from "../repositories/player_repository";
 import * as ReplayRepository from "../repositories/replay_repository";
 import { aMockGameWith, aMockPlayerWith, aMockReplayWith } from "./mocks";
-import * as TestRepository from "./test_repository";
 
 describe("database integration tests", () => {
   let db: Kysely<Database>;
@@ -19,7 +18,9 @@ describe("database integration tests", () => {
 
   afterEach(async () => {
     // Clear the database after each test
-    await TestRepository.clearDatabase(db);
+    await db.deleteFrom("replay").execute();
+    await db.deleteFrom("game").execute();
+    await db.deleteFrom("player").execute();
   });
 
   it("should disallow replays with the same folder and filename", async () => {
@@ -27,8 +28,7 @@ describe("database integration tests", () => {
     await ReplayRepository.insertReplay(db, aMockReplayWith({ folder: "folder", file_name: "different_name" }));
     await ReplayRepository.insertReplay(db, aMockReplayWith({ folder: "different_folder", file_name: "name" }));
 
-    const numReplays = await TestRepository.getRowCount(db, "replay");
-    expect(numReplays).toEqual(3);
+    expect(await getRowCount(db, "replay")).toEqual(3);
 
     const result = ReplayRepository.insertReplay(db, aMockReplayWith({ folder: "folder", file_name: "name" }));
     await expect(result).rejects.toThrowError();
@@ -36,31 +36,31 @@ describe("database integration tests", () => {
 
   it("should delete games when replays are deleted", async () => {
     const { replayId } = await addMockGame();
-    expect(await TestRepository.getRowCount(db, "replay")).toEqual(1);
-    expect(await TestRepository.getRowCount(db, "game")).toEqual(1);
+    expect(await getRowCount(db, "replay")).toEqual(1);
+    expect(await getRowCount(db, "game")).toEqual(1);
 
     await ReplayRepository.deleteReplayById(db, replayId);
-    expect(await TestRepository.getRowCount(db, "replay")).toEqual(0);
-    expect(await TestRepository.getRowCount(db, "game")).toEqual(0);
+    expect(await getRowCount(db, "replay")).toEqual(0);
+    expect(await getRowCount(db, "game")).toEqual(0);
   });
 
   it("should delete players when games are deleted", async () => {
     const { gameId } = await addMockGame();
-    expect(await TestRepository.getRowCount(db, "game")).toEqual(1);
+    expect(await getRowCount(db, "game")).toEqual(1);
 
     await PlayerRepository.insertPlayer(db, aMockPlayerWith(gameId));
-    expect(await TestRepository.getRowCount(db, "player")).toEqual(1);
+    expect(await getRowCount(db, "player")).toEqual(1);
 
-    await TestRepository.deleteGameById(db, gameId);
-    expect(await TestRepository.getRowCount(db, "game")).toEqual(0);
-    expect(await TestRepository.getRowCount(db, "player")).toEqual(0);
+    await db.deleteFrom("game").where("_id", "=", gameId).execute();
+    expect(await getRowCount(db, "game")).toEqual(0);
+    expect(await getRowCount(db, "player")).toEqual(0);
   });
 
   it("should disallow adding the same player for the same game", async () => {
     const { gameId } = await addMockGame();
     await PlayerRepository.insertPlayer(db, aMockPlayerWith(gameId, { index: 0 }));
     await PlayerRepository.insertPlayer(db, aMockPlayerWith(gameId, { index: 1 }));
-    expect(await TestRepository.getRowCount(db, "player")).toEqual(2);
+    expect(await getRowCount(db, "player")).toEqual(2);
 
     const result = PlayerRepository.insertPlayer(db, aMockPlayerWith(gameId, { index: 1 }));
     await expect(result).rejects.toThrowError();
@@ -84,4 +84,12 @@ async function createDatabase(): Promise<Kysely<Database>> {
   const migrationsFolder = path.join(__dirname, "../migrations");
   await migrateToLatest(database, migrationsFolder);
   return database;
+}
+
+async function getRowCount(db: Kysely<Database>, table: keyof Database): Promise<number> {
+  const { num_rows } = await db
+    .selectFrom(table)
+    .select((eb) => eb.fn.countAll<number>().as("num_rows"))
+    .executeTakeFirstOrThrow();
+  return num_rows;
 }
