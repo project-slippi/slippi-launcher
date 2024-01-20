1c88d6090d624bc5e0afe9794a108b8939479cab
Vince Au <vinceau09@gmail.com>
2024-01-20 23:39:41 +1100

    actually nah lets not use tiny-invariant

--- src/common/preconditions.ts
+++ src/common/preconditions.ts
@@ -1,11 +1,14 @@
-import invariant from "tiny-invariant";
-
 export class Preconditions {
   public static checkExists<T>(value: T | null | undefined, message?: string): asserts value is T {
     return this.checkState(value != null, message);
   }
 
   public static checkState(condition: any, message?: string): asserts condition {
-    invariant(condition, message);
+    if (condition) {
+      return;
+    }
+
+    // The required condition was not satisfied
+    throw new Error(message);
   }
 }
