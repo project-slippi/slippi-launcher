0299d715f8fc30b76f5e7f8f39099e1d25584600
Vince Au <vinceau09@gmail.com>
2024-01-20 19:06:54 +1100

    update condition

--- src/common/preconditions.ts
+++ src/common/preconditions.ts
@@ -2,6 +2,6 @@ import invariant from "tiny-invariant";
 
 export class Preconditions {
   public static checkExists<T>(value: T | null | undefined, message?: string): asserts value is T {
-    invariant(value, message);
+    invariant(value != null, message);
   }
 }
