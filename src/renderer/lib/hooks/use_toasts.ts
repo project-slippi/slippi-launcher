0c9444bc826aba0a4dfd434d4c0e2e2bb62e3c31
Vince Au <vinceau09@gmail.com>
2024-01-20 12:53:05 +1100

    use notification service in useToasts

--- src/renderer/lib/hooks/use_toasts.ts
+++ src/renderer/lib/hooks/use_toasts.ts
@@ -1,27 +1,6 @@
-import { useSnackbar } from "notistack";
-import React from "react";
+import { useServices } from "@/services";
 
 export const useToasts = () => {
-  const { enqueueSnackbar } = useSnackbar();
-  const toastHandlers = React.useMemo(
-    () => ({
-      showError: (error: Error | string | unknown) => {
-        let message: string;
-        if (error instanceof Error) {
-          message = error.message;
-        } else if (typeof error === "string") {
-          message = error;
-        } else {
-          message = JSON.stringify(error);
-        }
-        // Let's not automatically hide error messages
-        enqueueSnackbar(message, { variant: "error", persist: true });
-      },
-      showSuccess: (message: string) => enqueueSnackbar(message, { variant: "success", autoHideDuration: 2500 }),
-      showWarning: (message: string) => enqueueSnackbar(message, { variant: "warning" }),
-      showInfo: (message: string) => enqueueSnackbar(message, { variant: "info" }),
-    }),
-    [enqueueSnackbar],
-  );
-  return toastHandlers;
+  const { notificationService } = useServices();
+  return notificationService;
 };
