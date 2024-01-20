9824c4657b21e89c98ae8d0072e4b5928f9fae44
Vince Au <vinceau09@gmail.com>
2024-01-20 23:41:44 +1100

    add some credit

--- src/common/preconditions.ts
+++ src/common/preconditions.ts
@@ -1,3 +1,6 @@
+// Based on tiny-invariant
+// https://github.com/alexreardon/tiny-invariant/blob/619da0f9119558cd57aeff1ba5d022cad74f9bc7/src/tiny-invariant.ts
+
 export class Preconditions {
   public static checkExists<T>(value: T | null | undefined, message?: string): asserts value is T {
     return this.checkState(value != null, message);
