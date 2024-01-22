ccc22cf565d169f7e18ddb14783ee63191ade16f
Vince Au <vinceau09@gmail.com>
2024-01-22 20:56:44 +1100

    refactor: news feed

--- src/main/github.ts
+++ src/main/github.ts
@@ -8,6 +8,15 @@ const MINUTE = 60 * SECOND;
 
 const EXPIRES_IN = 10 * MINUTE;
 
+export type GithubReleaseInfo = {
+  id: number;
+  name: string;
+  body: string;
+  published_at: string;
+  html_url: string;
+  tag_name: string;
+};
+
 // Let's cache our Github responses so we don't exceed the 60 requests/hour rate limit
 type ResponseCacheEntry = {
   time: number;
@@ -16,7 +25,7 @@ type ResponseCacheEntry = {
 
 const githubResponseCache: Record<string, ResponseCacheEntry> = {};
 
-export async function getLatestRelease(owner: string, repo: string): Promise<any> {
+export async function getLatestRelease(owner: string, repo: string): Promise<GithubReleaseInfo> {
   // We can re-use api calls by returning the first item in all releases
   // since it already returns the newest release first.
   const data = await getAllReleases(owner, repo);
@@ -26,7 +35,7 @@ export async function getLatestRelease(owner: string, repo: string): Promise<any
   throw new Error(`No releases found for ${owner}/${repo}`);
 }
 
-export async function getAllReleases(owner: string, repo: string): Promise<any> {
+export async function getAllReleases(owner: string, repo: string): Promise<GithubReleaseInfo[]> {
   const url = `https://api.github.com/repos/${owner}/${repo}/releases`;
   const data = await cachedFetch(url);
   return data;
--- src/main/newsFeed.ts
+++ src/main/newsFeed.ts
@@ -1,4 +1,5 @@
 import type { NewsItem } from "@common/types";
+import log from "electron-log";
 import mediumJSONFeed from "medium-json-feed";
 
 import { getAllReleases } from "./github";
@@ -17,12 +18,12 @@ export async function fetchNewsFeedData(): Promise<NewsItem[]> {
 
 async function fetchMediumNews(): Promise<NewsItem[]> {
   const response = await mediumJSONFeed("project-slippi");
-  if (!response || response.status !== 200) {
+  if (response?.status !== 200) {
     throw new Error("Error fetching Medium feed");
   }
 
   const result = response.response;
-  return result.map((post: any) => {
+  return result.map((post: any): NewsItem => {
     const publishedAt = new Date(post.firstPublishedAt).toISOString();
     return {
       id: `medium-${post.id}`,
@@ -31,25 +32,32 @@ async function fetchMediumNews(): Promise<NewsItem[]> {
       subtitle: post.virtuals.subtitle,
       publishedAt,
       permalink: `https://medium.com/project-slippi/${post.uniqueSlug}`,
-    } as NewsItem;
+    };
   });
 }
 
 async function fetchGithubReleaseNews(repos: string[]): Promise<NewsItem[]> {
-  const allReleases = await Promise.all(
+  const allReleases = await Promise.allSettled(
     repos.map(async (repo) => {
       const releases = await getAllReleases("project-slippi", repo);
-      return releases.map((release: any) => {
+      return releases.map((release): NewsItem => {
         return {
           id: `gh-${repo}-${release.id}`,
           title: `[${repo}] ${release.name}`,
           body: release.body,
           publishedAt: release.published_at,
           permalink: release.html_url,
-        } as NewsItem;
+        };
       });
     }),
   );
 
-  return allReleases.flat();
+  return allReleases.flatMap((promise): NewsItem[] => {
+    if (promise.status === "fulfilled") {
+      return promise.value;
+    }
+
+    log.error(promise.reason);
+    return [];
+  });
 }
