3fec37f42dbf446e384315cbb8f008894198f812
Vince Au <vinceau09@gmail.com>
2024-04-15 19:26:17 +1000

    chore: add plugin for disabling lodash/chain

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
     tsconfigRootDir: __dirname,
     createDefaultProgram: true,
   },
-  plugins: ["simple-import-sort", "strict-booleans", "react-hooks", "prettier", "@stylexjs"],
+  plugins: ["simple-import-sort", "strict-booleans", "react-hooks", "prettier", "@stylexjs", "lodash"],
   extends: [
     "plugin:react/recommended",
     "eslint:recommended",
@@ -101,6 +101,7 @@ module.exports = {
     "react/jsx-boolean-value": ["error", "always"],
     "react/no-unstable-nested-components": "error",
     "@stylexjs/valid-styles": "error",
+    "lodash/chaining": "error",
   },
   ignorePatterns: ["/*.js", "node_modules"],
 };
