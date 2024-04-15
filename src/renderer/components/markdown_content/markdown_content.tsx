08e98a2740d79460451507ded6ccc60b24110951
Vince Au <vinceau09@gmail.com>
2024-04-15 22:45:25 +1000

    add code block to markdown content

--- src/renderer/components/markdown_content/markdown_content.tsx
+++ src/renderer/components/markdown_content/markdown_content.tsx
@@ -4,12 +4,17 @@ import ReactMarkdown from "react-markdown";
 import { ExternalLink as A } from "@/components/external_link";
 import { withFont } from "@/styles/with_font";
 
+import { CodeBlock } from "./code_block";
+
 export const MarkdownContent = ({ content, className }: { className?: string; content: string }) => {
   return (
     <Outer className={className}>
       <ReactMarkdown
         skipHtml={true}
         renderers={{
+          code: ({ value }: { value: string }) => {
+            return <CodeBlock content={value} />;
+          },
           link: ({ href, children }) => (
             <A href={href} title={href}>
               {children}
