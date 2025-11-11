1a2d4daf768c6730f8886bacccf2107030658925
Vince Au <vinceau09@gmail.com>
2025-11-12 00:16:05 +1100

    add gecko code messages

---
+++ src/renderer/pages/settings/dolphin_settings/gecko_codes/manage_codes/manage_codes.messages.ts
@@ -0,0 +1,7 @@
+export const ManageCodesMessages = {
+  deleteCode: () => "Delete Code",
+  copyToClipboard: () => "Copy to Clipboard",
+  codeWillBeDeleted: (codeName: string) => `The code <{0}> will be deleted. This cannot be undone.`,
+  areYouSure: () => "Are you sure you want to delete this code?",
+  delete: () => "Delete",
+};
