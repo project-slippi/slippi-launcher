78dc149d33cac5724df5f15460a57b5d4da6526c
Vince Au <vinceau09@gmail.com>
2025-11-12 00:22:17 +1100

    add messages for multipath input

---
+++ src/renderer/components/multi_path_input/multi_path_input.messages.ts
@@ -0,0 +1,7 @@
+export const MultiPathInputMessages = {
+  noAdditionalDirectoriesAdded: () => "No additional directories added.",
+  add: () => "Add",
+  remove: () => "Remove",
+  cannotAddSubdirectoriesOfRoot: () => "Cannot add sub directories of the Root SLP Directory.",
+  thatDirectoryIsAlreadyIncluded: () => "That directory is already included.",
+};
