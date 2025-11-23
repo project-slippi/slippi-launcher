a179d0d011b9c9a8cadc0289f4212056a2669ba2
Vince Au <vinceau09@gmail.com>
2025-11-23 16:28:40 +1100

    add more messages for replay

---
+++ src/renderer/pages/replays/replay_file_stats/replay_file_stats.messages.ts
@@ -0,0 +1,10 @@
+export const ReplayFileStatsMessages = {
+  uhOhWeCouldntOpenThatFile: () => "Uh oh. We couldn't open that file. It's probably corrupted.",
+  goBack: () => "Go back",
+  loading: () => "Loading...",
+  crunchingNumbers: () => "Crunching numbers...",
+  noStatsComputed: () => "No stats computed",
+  gameStatsForDoublesIsUnsupported: () => "Game stats for doubles is unsupported",
+  revealLocation: () => "Reveal location",
+  currentFile: () => "Current file",
+};
