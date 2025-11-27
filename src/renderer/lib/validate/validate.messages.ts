e2a8c32a4ca520d8701ad758581301ab0060f8d3
Vince Au <vinceau09@gmail.com>
2025-11-27 11:42:09 +1100

    add form validation error messages

---
+++ src/renderer/lib/validate/validate.messages.ts
@@ -0,0 +1,11 @@
+export const ValidateMessages = {
+  invalidIpAddress: () => "Invalid IP address",
+  invalidCode: () => "Invalid code",
+  onlyEnglishCharactersAllowed: () => "Only English characters are allowed",
+  codeIsTooShort: () => "Code is too short",
+  codeIsTooLong: () => "Code is too long",
+  displayNamesCanOnlyContain: () =>
+    "Display names can only contain letters, numbers, Hiragana, Katakana, and special characters",
+  displayNameIsTooShort: () => "Display name is too short",
+  displayNameIsTooLong: () => "Display name is too long",
+};
