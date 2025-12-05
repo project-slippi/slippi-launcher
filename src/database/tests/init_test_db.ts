b5f4502576297a8b02d4bc4d868d2cf968853cb9
Vince Au <vinceau09@gmail.com>
2025-12-05 14:06:05 +1100

    add missing init_test_db.ts file

---
+++ src/database/tests/init_test_db.ts
@@ -0,0 +1,18 @@
+import type { Database } from "@database/schema";
+import Sqlite from "better-sqlite3";
+import { Kysely, SqliteDialect } from "kysely";
+
+import webpackPaths from "../../../.erb/configs/webpack.paths";
+import { migrateToLatest } from "../migrate_to_latest";
+
+export async function initTestDb(): Promise<Kysely<Database>> {
+  const sqliteDb = new Sqlite(":memory:");
+  const database = new Kysely<Database>({
+    dialect: new SqliteDialect({
+      database: sqliteDb,
+    }),
+  });
+
+  await migrateToLatest(database, webpackPaths.distMigrationsPath);
+  return database;
+}
