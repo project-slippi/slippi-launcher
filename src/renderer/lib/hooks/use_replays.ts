2be4e99f79dc0254f6adb33dc87f1b68d8578c50
Vince Au <vinceau09@gmail.com>
2025-12-10 14:46:19 +1100

    increase batch size for less fetching

--- src/renderer/lib/hooks/use_replays.ts
+++ src/renderer/lib/hooks/use_replays.ts
@@ -9,7 +9,7 @@ import { useServices } from "@/services";
 import { useReplayBrowserList } from "./use_replay_browser_list";
 import { buildReplayFilters, useReplayFilter } from "./use_replay_filter";
 
-const REPLAY_BATCH_SIZE = 20;
+const REPLAY_BATCH_SIZE = 50;
 
 type StoreState = {
   loading: boolean;
