d16c02018edee7d7bd0ee7a8377bce9602d5c283
Jas Laferriere <Fizzi36@gmail.com>
2026-01-14 16:38:49 -0500

    fix: use correct condition for auth lock
    regression https://github.com/project-slippi/slippi-launcher/commit/09aee747695d6b47f6df16075f5153d2a15506da#diff-39bf032e2acc0be1c5d434bfe3cd62e02f278ce42850f72f1cd0a81e0eafd02eR9

--- src/renderer/components/auth_guard.tsx
+++ src/renderer/components/auth_guard.tsx
@@ -6,7 +6,7 @@ import { LoginNotice } from "./login_notice/login_notice";
 
 export const AuthGuard = ({ children }: { children: React.ReactNode }) => {
   const user = useAccount((store) => store.user);
-  if (user) {
+  if (!user) {
     return <LoginNotice />;
   }
   return <>{children}</>;
