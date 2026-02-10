418182c8acaaac305890d754f33541f17bc808db
Vince <vinceau09@gmail.com>
2026-02-10 10:00:07 +0900

    use enum for aliases

--- src/renderer/lib/query_language/aliases.ts
+++ src/renderer/lib/query_language/aliases.ts
@@ -1,27 +1,19 @@
 // Define additional aliases for characters and stages here
 
+import { Character, Stage } from "@slippi/slippi-js";
+
 export const STAGE_ALIASES = new Map<number, string[]>([
-  // Fountain of Dreams
-  [2, ["FoD", "Fountain"]],
-  // Pokémon Stadium
-  [3, ["PS", "Stadium"]],
-  // Yoshi's Story
-  [8, ["YS"]],
-  // Dream Land N64
-  [28, ["DL", "Dreamland"]],
-  // Battlefield
-  [31, ["BF"]],
-  // Final Destination
-  [32, ["FD"]],
+  [Stage.FOUNTAIN_OF_DREAMS, ["FoD", "Fountain"]],
+  [Stage.POKEMON_STADIUM, ["PS", "Stadium"]],
+  [Stage.YOSHIS_STORY, ["YS"]],
+  [Stage.DREAMLAND, ["DL", "Dreamland"]],
+  [Stage.BATTLEFIELD, ["BF"]],
+  [Stage.FINAL_DESTINATION, ["FD"]],
 ]);
 
 export const CHARACTER_ALIASES = new Map<number, string[]>([
-  // Captain Falcon
-  [0, ["CF"]],
-  // Game & Watch
-  [3, ["GnW"]],
-  // Mewtwo
-  [10, ["M2"]],
-  // Young Link
-  [21, ["YL"]],
+  [Character.CAPTAIN_FALCON, ["CF"]],
+  [Character.GAME_AND_WATCH, ["GnW"]],
+  [Character.MEWTWO, ["M2"]],
+  [Character.YOUNG_LINK, ["YL"]],
 ]);
