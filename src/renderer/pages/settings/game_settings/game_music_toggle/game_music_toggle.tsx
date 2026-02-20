592094f548267ca0589645493f31506ad46e3ca3
Nikhil Narayana <nikhil.narayana@live.com>
2026-02-19 23:32:32 -0800

    undo unnecessary music toggle change

--- src/renderer/pages/settings/game_settings/game_music_toggle/game_music_toggle.tsx
+++ src/renderer/pages/settings/game_settings/game_music_toggle/game_music_toggle.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { gte } from "semver";
 
 import { Toggle } from "@/components/form/toggle";
 import { useDolphinStore } from "@/lib/dolphin/use_dolphin_store";
@@ -7,6 +8,8 @@ import { useEnableJukebox } from "@/lib/hooks/use_settings";
 import { SettingItem } from "../../setting_item_section";
 import { GameMusicToggleMessages as Messages } from "./game_music_toggle.messages";
 
+const MIN_DOLPHIN_VERSION_FOR_JUKEBOX = "3.2.0";
+
 const isWindows = window.electron.bootstrap.isWindows;
 
 export const GameMusicToggle = () => {
@@ -14,20 +17,24 @@ export const GameMusicToggle = () => {
   const netplayDolphinOpen = useDolphinStore((store) => store.netplayOpened);
   const dolphinVersion = useDolphinStore((store) => store.netplayDolphinVersion);
 
-  const disabled = netplayDolphinOpen || !dolphinVersion;
+  const isJukeboxCompatible = dolphinVersion != null && gte(dolphinVersion, MIN_DOLPHIN_VERSION_FOR_JUKEBOX);
+  const disabled = netplayDolphinOpen || !isJukeboxCompatible || !dolphinVersion;
 
   const jukeboxDescription = React.useMemo(() => {
     const descriptions = [Messages.enableMusicDescription()];
     if (isWindows) {
       descriptions.push(Messages.incompatibleWithWasapi());
     }
+    if (dolphinVersion && !isJukeboxCompatible) {
+      descriptions.push(Messages.requiresDolphinVersion(MIN_DOLPHIN_VERSION_FOR_JUKEBOX));
+    }
     return descriptions.join(" ");
-  }, []);
+  }, [dolphinVersion, isJukeboxCompatible]);
 
   return (
     <SettingItem name="">
       <Toggle
-        value={enableJukebox}
+        value={isJukeboxCompatible && enableJukebox}
         onChange={(checked) => setEnableJukebox(checked)}
         label={Messages.enableMusic()}
         description={jukeboxDescription}
