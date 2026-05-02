956e144366ef24a7e29e9da2c40466336ebabecd
Vince Au <vinceau09@gmail.com>
2026-05-02 11:55:09 +0900

    use css in bouncing slippi logo

--- src/renderer/components/bouncing_slippi_logo/bouncing_slippi_logo.tsx
+++ src/renderer/components/bouncing_slippi_logo/bouncing_slippi_logo.tsx
@@ -1,43 +1,9 @@
-import * as stylex from "@stylexjs/stylex";
+import { clsx } from "clsx";
 import React from "react";
 
 import slippiLogo from "@/styles/images/slippi_logo.svg";
 
-const bounceAnimation = stylex.keyframes({
-  "0%": { bottom: 0 },
-  "100%": { bottom: 25 },
-});
-
-const barrelRollAnimation = stylex.keyframes({
-  "0%": { transform: "rotate(0)" },
-  "100%": { transform: "rotate(720deg)" },
-});
-
-const styles = stylex.create({
-  container: {
-    display: "flex",
-    position: "relative",
-    paddingTop: 20,
-    height: 80,
-    width: 80,
-  },
-  logo: (logoUrl: string) => ({
-    backgroundImage: `url("${logoUrl}")`,
-    backgroundSize: "contain",
-    backgroundRepeat: "no-repeat",
-    position: "absolute",
-    height: 60,
-    width: 80,
-  }),
-  onlyBounce: {
-    // eslint-disable-next-line @stylexjs/valid-styles
-    animation: `${bounceAnimation} 0.6s infinite alternate`,
-  },
-  bouncePlusSpin: {
-    // eslint-disable-next-line @stylexjs/valid-styles
-    animation: `${bounceAnimation} 0.6s infinite alternate, ${barrelRollAnimation} 1s cubic-bezier(0.68, -0.55, 0.265, 1.55) alternate forwards`,
-  },
-});
+import styles from "./bouncing_slippi_logo.module.scss";
 
 export const BouncingSlippiLogo = () => {
   const ref = React.createRef<HTMLDivElement>();
@@ -63,14 +29,15 @@ export const BouncingSlippiLogo = () => {
   }, [animationState, setAnimationState]);
 
   return (
-    <div {...stylex.props(styles.container)}>
+    <div className={styles.container}>
       <div
-        {...stylex.props(
-          styles.logo(slippiLogo),
-          animationState === "ready" ? styles.onlyBounce : styles.bouncePlusSpin,
-        )}
+        className={clsx(styles.logo, {
+          [styles.onlyBounce]: animationState === "ready",
+          [styles.bouncePlusSpin]: animationState === "running",
+        })}
         ref={ref}
         onMouseOver={onMouseOver}
+        style={{ backgroundImage: `url("${slippiLogo}")` }}
       />
     </div>
   );
