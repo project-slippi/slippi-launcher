aa902465a96cddd4651990d375b7addd84410049
Vince Au <vinceau09@gmail.com>
2026-05-02 12:14:00 +0900

    migrate stylex to css

--- src/renderer/components/markdown_content/code_block.tsx
+++ src/renderer/components/markdown_content/code_block.tsx
@@ -1,42 +1,10 @@
 import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 import IconButton from "@mui/material/IconButton";
 import Tooltip from "@mui/material/Tooltip";
-import stylex from "@stylexjs/stylex";
+import { clsx } from "clsx";
 import React from "react";
 
-const styles = stylex.create({
-  inlineContainer: {
-    padding: "2px 6px",
-    backgroundColor: "rgba(0, 0, 0, 0.5)",
-    borderRadius: 5,
-    position: "relative",
-  },
-  container: {
-    fontSize: 15,
-    padding: "20px 30px",
-    paddingRight: "50px",
-    backgroundColor: "rgba(0, 0, 0, 0.5)",
-    borderRadius: 5,
-    position: "relative",
-  },
-  code: {
-    color: "#999",
-    lineHeight: "1.5em",
-    whiteSpace: "pre-wrap",
-    overflowWrap: "break-word",
-    wordBreak: "break-word",
-  },
-  buttonContainer: {
-    position: "absolute",
-    top: 10,
-    right: 10,
-    opacity: 0,
-    transition: "opacity 0.1s ease-in-out",
-  },
-  visible: {
-    opacity: 1,
-  },
-});
+import styles from "./code_block.module.scss";
 
 const StandaloneCodeBlock = React.memo(function StandaloneCodeBlock({ content }: { content: string }) {
   const [copied, setCopied] = React.useState<boolean>(false);
@@ -53,23 +21,23 @@ const StandaloneCodeBlock = React.memo(function StandaloneCodeBlock({ content }:
       .catch(console.error);
   };
   return (
-    <div {...stylex.props(styles.container)} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
-      <div {...stylex.props(styles.buttonContainer, isHovering && styles.visible)}>
+    <div className={styles.container} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
+      <div className={clsx(styles.buttonContainer, isHovering && styles.visible)}>
         <Tooltip title={copied ? "Copied!" : "Copy to clipboard"}>
           <IconButton onClick={onCopy} size="small">
             <ContentCopyIcon fontSize="small" />
           </IconButton>
         </Tooltip>
       </div>
-      <code {...stylex.props(styles.code)}>{content}</code>
+      <code className={styles.code}>{content}</code>
     </div>
   );
 });
 
 const InlineCodeBlock = React.memo(function InlineCodeBlock({ content }: { content: string }) {
   return (
-    <span {...stylex.props(styles.inlineContainer)}>
-      <code {...stylex.props(styles.code)}>{content}</code>
+    <span className={styles.inlineContainer}>
+      <code className={styles.code}>{content}</code>
     </span>
   );
 });
