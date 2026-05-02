93c15f0280d183b3211fc43bc3f55d3817cf428b
Vince Au <vinceau09@gmail.com>
2026-05-02 13:35:35 +0900

    start migrating to css variables

--- src/renderer/styles/colors.ts
+++ src/renderer/styles/colors.ts
@@ -24,3 +24,30 @@ export const colors = {
   textDim: "#939599",
   textVeryDim: "#5E6066",
 } as const;
+
+const cssVars = {
+  greenPrimary: "--green-primary",
+  purplePrimary: "--purple-primary",
+
+  grayDark: "--gray-dark",
+  greenDark: "--green-dark",
+  greenDarker: "--green-darker",
+  purpleLighter: "--purple-lighter",
+  purpleLight: "--purple-light",
+  purple: "--purple",
+  purpleDark: "--purple-dark",
+  purpleDarker: "--purple-darker",
+  offGray: "--off-gray",
+  offWhite: "--off-white",
+
+  textPrimary: "--text-primary",
+  textSecondary: "--text-secondary",
+  textDim: "--text-dim",
+  textVeryDim: "--text-very-dim",
+} as const;
+
+type CssVarKey = keyof typeof cssVars;
+
+export function cssVar(name: CssVarKey) {
+  return `var(${cssVars[name]})`;
+}
