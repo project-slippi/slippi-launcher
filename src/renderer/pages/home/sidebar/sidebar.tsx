f871668c0fc390150ce792daecea2b042505c518
Vince Au <vinceau09@gmail.com>
2026-05-02 14:08:25 +0900

    add sidebar css

--- src/renderer/pages/home/sidebar/sidebar.tsx
+++ src/renderer/pages/home/sidebar/sidebar.tsx
@@ -1,24 +1,10 @@
-import * as stylex from "@stylexjs/stylex";
-
-import { colors } from "@/styles/tokens.stylex";
-
 import { RankedStatus } from "./ranked_status";
+import styles from "./sidebar.module.css";
 import { TournamentLinks } from "./tournament_links";
-
-const styles = stylex.create({
-  container: {
-    display: "flex",
-    flexDirection: "column",
-    flex: "1",
-    position: "relative",
-    overflow: "hidden",
-    backgroundColor: colors.purpleDarker,
-  },
-});
 
 export const Sidebar = () => {
   return (
-    <div {...stylex.props(styles.container)}>
+    <div className={styles.container}>
       <RankedStatus />
       <TournamentLinks />
     </div>
