05721fde0e5a1d4be3fe54a5a4badcdb957e2f46
Vince Au <vinceau09@gmail.com>
2026-05-07 10:10:52 +0900

    simplify overview styling

--- src/renderer/pages/home/overview/overview.tsx
+++ src/renderer/pages/home/overview/overview.tsx
@@ -12,26 +12,14 @@ export const HomeOverview = React.memo(function HomeOverview() {
   const navigateToHomeTab = useHomeNavigation();
   return (
     <div className={styles.container}>
-      <div className={styles.leftCol}>
-        <div className={styles.col1}>
-          <ContentBlock
-            title={Messages.latestNews()}
-            content={<NewsPreview />}
-            onClick={() => navigateToHomeTab("news")}
-          />
-        </div>
-        <div className={styles.col2}>
-          <ContentBlock
-            title={Messages.upcomingTournaments()}
-            content={<MeleeMajorsCarousel />}
-            onClick={() => navigateToHomeTab("tournaments")}
-          />
-        </div>
-      </div>
-      <div className={styles.rightCol}>
-        <div>
-          <ContentBlock title={Messages.rankedDay()} content={<RankedStatus />} />
-        </div>
+      <ContentBlock title={Messages.latestNews()} content={<NewsPreview />} onClick={() => navigateToHomeTab("news")} />
+      <ContentBlock
+        title={Messages.upcomingTournaments()}
+        content={<MeleeMajorsCarousel />}
+        onClick={() => navigateToHomeTab("tournaments")}
+      />
+      <div>
+        <ContentBlock title={Messages.rankedDay()} content={<RankedStatus />} />
       </div>
     </div>
   );
