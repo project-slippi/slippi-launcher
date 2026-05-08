04c84566092724cb97ec22a3cd5559751abfa9e8
Vince Au <vinceau09@gmail.com>
2026-05-08 17:37:18 +0900

    remove unused message

--- src/renderer/pages/home/overview/overview.messages.ts
+++ src/renderer/pages/home/overview/overview.messages.ts
@@ -2,5 +2,4 @@ export const OverviewMessages = {
   latestNews: () => "Latest News",
   upcomingTournaments: () => "Upcoming Tournaments",
   rankedDay: () => "Ranked Day",
-  myRanking: () => "My Ranking",
 };
