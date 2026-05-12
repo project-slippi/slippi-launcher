1c5f134c426d2648ed64cd2fdb8b689b80b23649
Vince Au <vinceau09@gmail.com>
2026-05-12 14:21:29 +0900

    fix: unstable queryFn causing duplicate requests

--- src/renderer/lib/hooks/use_data_fetch_query.ts
+++ src/renderer/lib/hooks/use_data_fetch_query.ts
@@ -6,7 +6,7 @@ const HOUR = 60 * MINUTE;
 export const useNewsFeedQuery = () => {
   return useQuery({
     queryKey: ["newsFeedQuery"],
-    queryFn: window.electron.common.fetchNewsFeed,
+    queryFn: () => window.electron.common.fetchNewsFeed(),
     // Probably won't change that much so this should be fine
     staleTime: 15 * MINUTE,
   });
@@ -15,7 +15,7 @@ export const useNewsFeedQuery = () => {
 export const useMeleeMajorsQuery = () => {
   return useQuery({
     queryKey: ["meleeMajorsQuery"],
-    queryFn: window.electron.fetch.fetchUpcomingMeleeMajors,
+    queryFn: () => window.electron.fetch.fetchUpcomingMeleeMajors(),
     // The melee majors list rarely ever changes so we probably don't need to fetch often at all
     staleTime: 24 * HOUR,
   });
