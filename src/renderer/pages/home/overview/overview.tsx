21866f90d7e23a5e91d39cb998a88107601559ff
Vince Au <vinceau09@gmail.com>
2026-05-12 17:41:39 +0900

    don't navigate tabs via content block header

--- src/renderer/pages/home/overview/overview.tsx
+++ src/renderer/pages/home/overview/overview.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 
-import { HomeRoutes } from "../home_routes";
 import { ContentBlock } from "./content_block/content_block";
 import { MeleeMajorsCarousel } from "./melee_majors_carousel";
 import { NewsPreview } from "./news_preview/news_preview";
@@ -10,19 +8,10 @@ import styles from "./overview.module.css";
 import { RankedStatus } from "./ranked_status/ranked_status";
 
 export const HomeOverview = React.memo(function HomeOverview() {
-  const navigate = useNavigate();
   return (
     <div className={styles.container}>
-      <ContentBlock
-        title={Messages.latestNews()}
-        content={<NewsPreview />}
-        onClick={() => navigate(HomeRoutes.latestNews())}
-      />
-      <ContentBlock
-        title={Messages.upcomingTournaments()}
-        content={<MeleeMajorsCarousel />}
-        onClick={() => navigate(HomeRoutes.upcomingTournaments())}
-      />
+      <ContentBlock title={Messages.latestNews()} content={<NewsPreview />} />
+      <ContentBlock title={Messages.upcomingTournaments()} content={<MeleeMajorsCarousel />} />
       <div>
         <ContentBlock title={Messages.rankedDay()} content={<RankedStatus />} />
       </div>
