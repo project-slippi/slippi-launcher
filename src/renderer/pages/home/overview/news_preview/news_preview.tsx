a04f07a6ea4eab4ee412f0880c4757a8ae8cbd2a
Vince Au <vinceau09@gmail.com>
2026-05-12 16:57:54 +0900

    add custom news preview item

--- src/renderer/pages/home/overview/news_preview/news_preview.tsx
+++ src/renderer/pages/home/overview/news_preview/news_preview.tsx
@@ -1,10 +1,13 @@
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import Newspaper from "@mui/icons-material/Newspaper";
+import Button from "@mui/material/Button";
 import CircularProgress from "@mui/material/CircularProgress";
+import { clsx } from "clsx";
 import React from "react";
 
 import { useNewsFeedQuery } from "@/lib/hooks/use_data_fetch_query";
 
-import { NewsArticleContainer as NewsArticle } from "../../news_feed/news_article/news_article.container";
+import { ItemPreview } from "./item_preview/item_preview";
 import { NewsPreviewMessages as Messages } from "./news_preview.messages";
 import styles from "./news_preview.module.css";
 
@@ -13,7 +16,7 @@ export const NewsPreview = React.memo(function NewsPreview() {
 
   if (isLoading) {
     return (
-      <Outer>
+      <Outer centered={true}>
         <CircularProgress color="inherit" />
       </Outer>
     );
@@ -21,7 +24,7 @@ export const NewsPreview = React.memo(function NewsPreview() {
 
   if (error instanceof Error) {
     return (
-      <Outer>
+      <Outer centered={true}>
         <Newspaper style={{ fontSize: 64 }} />
         <h3>{Messages.failedToFetchNews()}</h3>
       </Outer>
@@ -30,7 +33,7 @@ export const NewsPreview = React.memo(function NewsPreview() {
 
   if (allPosts.length === 0) {
     return (
-      <Outer>
+      <Outer centered={true}>
         <Newspaper style={{ fontSize: 64 }} />
         <h3>{Messages.noNews()}</h3>
       </Outer>
@@ -39,13 +42,16 @@ export const NewsPreview = React.memo(function NewsPreview() {
 
   return (
     <Outer>
-      <div style={{ margin: 15, boxShadow: "4px 4px 8px rgba(0, 0, 0, 0.8)", overflow: "auto" }}>
-        <NewsArticle item={allPosts[0]} autoTruncate={true} />
+      <div className={styles.previewContainer}>
+        <ItemPreview item={allPosts.find((post) => post.source === "bluesky") || allPosts[0]} />
       </div>
+      <Button color="secondary" sx={{ width: "100%" }} size="medium" variant="outlined" endIcon={<ChevronRightIcon />}>
+        Read post
+      </Button>
     </Outer>
   );
 });
 
-const Outer = ({ children }: { children: React.ReactNode }) => {
-  return <div className={styles.container}>{children}</div>;
+const Outer = ({ children, centered }: { children: React.ReactNode; centered?: boolean }) => {
+  return <div className={clsx(styles.container, centered && styles.centered)}>{children}</div>;
 };
