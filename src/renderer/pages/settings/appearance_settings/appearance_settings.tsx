9ac792ca27b957f2483c78618dd2c66929ac4d3f
zaderrr <ellisbrown88@gmail.com>
2026-05-25 12:28:21 +0100

    Updated setting text + renamed toggle file

--- src/renderer/pages/settings/appearance_settings/appearance_settings.tsx
+++ src/renderer/pages/settings/appearance_settings/appearance_settings.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { LanguageSelector } from "./language_selector/language_selector";
-import { RankDisplayToggle } from "./rank_display_toggle/RankDisplayToggle";
+import { RankDisplayToggle } from "./rank_display_toggle/rank_display_toggle";
 
 export const AppearanceSettings = React.memo(() => {
   return (
--- src/renderer/pages/settings/appearance_settings/rank_display_toggle/rank_display_toggle.messages.ts
+++ src/renderer/pages/settings/appearance_settings/rank_display_toggle/rank_display_toggle.messages.ts
@@ -1,4 +1,4 @@
 export const RankDisplayToggleMessages = {
-  rankDisplay: () => "Rank display",
-  rankDisplayDescription: () => "Enable the current rank display on the home page.",
+  rankDisplay: () => "Show Ranking on Home Page",
+  rankDisplayDescription: () => "If enabled, your current rank and rating will be displayed on the Home page.",
 };
