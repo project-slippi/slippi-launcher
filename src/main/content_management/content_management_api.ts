e06e1b67668ae867961d4281902ba4d581811eef
Vince Au <vinceau09@gmail.com>
2026-06-01 20:22:00 +0900

    move api into content_management folder

---
+++ src/main/content_management/content_management_api.ts
@@ -0,0 +1,21 @@
+import { ipc_contentManagementService } from "../ipc";
+import type { ContentManagementParams, ContentManagementResult, ContentManagementServiceName } from "./registry";
+
+async function callService<N extends ContentManagementServiceName>(
+  service: N,
+  params: ContentManagementParams<N>,
+): Promise<ContentManagementResult<N>> {
+  const { result } = await ipc_contentManagementService.renderer!.trigger({
+    service,
+    params,
+  });
+  return result.data as ContentManagementResult<N>;
+}
+
+export default {
+  fetchCurrentLocation: (lang?: string) => callService("fetchCurrentLocation", { lang }),
+  fetchNearestTournaments: (location: { lat: number; lng: number }, radiusKms?: number) =>
+    callService("fetchNearestTournaments", { location, radiusKms }),
+  fetchUpcomingMeleeMajors: () => callService("fetchUpcomingMeleeMajors", {}),
+  fetchNewsFeed: () => callService("fetchNewsFeed", {}),
+};
