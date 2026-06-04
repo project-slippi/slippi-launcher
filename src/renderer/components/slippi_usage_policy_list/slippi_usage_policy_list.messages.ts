3c1ff3693ba8fcaef640e50675b2ccbe33ef1afc
Vince Au <vinceau09@gmail.com>
2026-06-04 14:57:02 +0900

    i18n: remove duplicate message

--- src/renderer/components/slippi_usage_policy_list/slippi_usage_policy_list.messages.ts
+++ src/renderer/components/slippi_usage_policy_list/slippi_usage_policy_list.messages.ts
@@ -1,6 +1,5 @@
 export const SlippiUsagePolicyListMessages = {
   privacyPolicyAndTermsOfService: () => "Privacy Policy and Terms of Service",
-  acceptSlippiRules: () => "I accept the Slippi Online Rules",
   clickTheLinksBelow: () => "Click the links below to review:",
   slippiPrivacyPolicy: () => "Slippi Privacy Policy",
   slippiTermsOfService: () => "Slippi Terms of Service",
