d148a8423f4d4d3433449beefa6ce50eecb3e862
Vince Au <vinceau09@gmail.com>
2026-06-04 09:00:49 +0900

    fix openings per kill condition

--- src/renderer/pages/replays/replay_file_stats/overall_table/build_sections.ts
+++ src/renderer/pages/replays/replay_file_stats/overall_table/build_sections.ts
@@ -99,7 +99,7 @@ export function buildSections(p1: PlayerStatSummary, p2: PlayerStatSummary): Sta
         simpleStat("Kills", p1.kills, p2.kills, (a, b) => a > b, String),
         simpleStat("Damage Done", p1.totalDamage, p2.totalDamage, (a, b) => a > b),
         fractionStat("Opening Conversion Rate", p1.successfulConversions, p2.successfulConversions, "higher"),
-        simpleStat("Openings / Kill", p1.openingsPerKill, p2.openingsPerKill, (a, b) => 0 < a && a < b),
+        simpleStat("Openings / Kill", p1.openingsPerKill, p2.openingsPerKill, (a, b) => a > 0 && (b === 0 || a < b)),
         simpleStat("Damage / Opening", p1.damagePerOpening, p2.damagePerOpening, (a, b) => a > b),
       ],
     },
