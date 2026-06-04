e5c2c508e631376c46cdb10ce69ba44e49d9bd3f
Vince Au <vinceau09@gmail.com>
2026-06-04 14:26:12 +0900

    remove unused messages

--- src/renderer/pages/quick_start/steps/accept_rules_step/accept_rules_step.messages.ts
+++ src/renderer/pages/quick_start/steps/accept_rules_step/accept_rules_step.messages.ts
@@ -1,12 +1,6 @@
 export const AcceptRulesStepMessages = {
-  slippiOnlineRules: () => "Slippi Online Rules",
-  privacyPolicyAndTermsOfService: () => "Privacy Policy and Terms of Service",
   acceptSlippiRules: () => "I accept the Slippi Online Rules",
-  clickTheLinksBelow: () => "Click the links below to review:",
-  slippiPrivacyPolicy: () => "Slippi Privacy Policy",
-  slippiTermsOfService: () => "Slippi Terms of Service",
   acceptPrivacyPolicyAndTos: () => "I accept the Slippi Privacy Policy and Terms of Service",
   acceptAll: () => "Accept All",
-  errorMissingUser: () => "An error occurred. The application does not have a user.",
   acceptRulesAndPolicies: () => "Accept rules and policies",
 };
