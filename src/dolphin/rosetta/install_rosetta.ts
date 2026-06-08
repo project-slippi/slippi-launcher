2c270eb4d02f3865ad3f99afa6ee394600cd20de
Vince Au <vinceau09@gmail.com>
2026-06-08 11:09:17 +0900

    gate rosetta install to macos

--- src/dolphin/rosetta/install_rosetta.ts
+++ src/dolphin/rosetta/install_rosetta.ts
@@ -1,6 +1,11 @@
 import { execFile } from "node:child_process";
 
 export async function installRosettaElevated() {
+  // This only makes sense on macOS
+  if (process.platform !== "darwin") {
+    return;
+  }
+
   return new Promise<number>((resolve) => {
     const script = `
       do shell script "/usr/sbin/softwareupdate --install-rosetta --agree-to-license" with administrator privileges
