8ba65f237d3f5f90719667668c7c10ec3fb6b431
Vince Au <vinceau09@gmail.com>
2026-06-08 14:56:27 +0900

    download the vcredist in js not via powershell script

--- src/dolphin/install_vcredist.ts
+++ src/dolphin/install_vcredist.ts
@@ -1,31 +1,40 @@
-// import { execFile } from "node:child_process";
-// import path from "node:path";
+import { app } from "electron";
+import log from "electron-log";
+import { execFile } from "node:child_process";
+import path from "node:path";
+import { download } from "utils/download";
 
-export async function installVcRedist() {
+const VCREDIST_URL = "https://aka.ms/vs/17/release/vc_redist.x64.exe";
+
+export async function installVcRedist(): Promise<number> {
   // This only makes sense on Windows
   if (process.platform !== "win32") {
     return 0;
   }
 
-  /*
-  const scriptPath = path.join(process.resourcesPath, "include", "ensure_vcredist.ps1");
+  const tmpDir = path.join(app.getPath("userData"), "temp");
+  const filename = path.basename(new URL(VCREDIST_URL).pathname);
+  const downloadPath = path.join(tmpDir, filename);
+
+  try {
+    await download({
+      url: VCREDIST_URL,
+      destinationFile: downloadPath,
+      overwrite: true,
+    });
+  } catch (err) {
+    log.error("Failed to download VC++ Redistributable", err);
+    return 1;
+  }
 
   return new Promise<number>((resolve) => {
-    execFile(
-      "powershell.exe",
-      ["-ExecutionPolicy", "Bypass", "-File", scriptPath],
-      { timeout: 5 * 60 * 1000 },
-      (err) => {
-        if (err) {
-          const code = typeof err.code === "number" ? err.code : 1;
-          resolve(code);
-          return;
-        }
-        resolve(0);
-      },
-    );
+    execFile(downloadPath, ["/install", "/passive", "/norestart"], { timeout: 5 * 60 * 1000 }, (err) => {
+      if (err) {
+        const code = typeof err.code === "number" ? err.code : 1;
+        resolve(code);
+        return;
+      }
+      resolve(0);
+    });
   });
-  */
-
-  return 0;
 }
