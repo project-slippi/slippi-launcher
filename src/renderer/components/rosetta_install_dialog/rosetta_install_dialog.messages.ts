5392e3f78e0e10569bc8ab37c58d4eacb4ec35d7
Vince Au <vinceau09@gmail.com>
2026-06-09 17:43:00 +0900

    i dont think i care about translating the error message

--- src/renderer/components/rosetta_install_dialog/rosetta_install_dialog.messages.ts
+++ src/renderer/components/rosetta_install_dialog/rosetta_install_dialog.messages.ts
@@ -2,6 +2,7 @@ export const RosettaInstallDialogMessages = {
   dialogTitle: () => "Rosetta Required",
   description: () => "Slippi Dolphin requires Rosetta on Apple Silicon Macs. Would you like to install it now?",
   installing: () => "Installing... This may take a few minutes.",
+  /** @noTranslate */
   installFailed: () =>
     `Rosetta installation failed. To manually install, open the Terminal app and run "softwareupdate --install-rosetta".`,
   installSuccess: () => "Installation completed successfully. You can now relaunch Slippi Dolphin.",
