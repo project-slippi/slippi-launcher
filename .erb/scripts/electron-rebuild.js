3f5b6f223f2eb6775f4d9e67121ecb754a116f34
Vince Au <vinceau09@gmail.com>
2026-06-20 14:15:55 +0900

    fix build again maybe

--- .erb/scripts/electron-rebuild.js
+++ .erb/scripts/electron-rebuild.js
@@ -1,13 +1,18 @@
 import { rebuild } from "@electron/rebuild";
 import fs from "fs";
+import path from "path";
 
 import { dependencies } from "../../release/app/package.json";
 import webpackPaths from "../configs/webpack.paths";
 
 void (async () => {
   if (Object.keys(dependencies || {}).length > 0 && fs.existsSync(webpackPaths.appNodeModulesPath)) {
+    const electronPkgPath = path.join(webpackPaths.rootPath, "node_modules", "electron", "package.json");
+    const electronVersion = JSON.parse(fs.readFileSync(electronPkgPath, "utf8")).version;
+
     await rebuild({
       buildPath: webpackPaths.appPath,
+      electronVersion,
       force: true,
       types: ["prod", "dev", "optional"],
     });
