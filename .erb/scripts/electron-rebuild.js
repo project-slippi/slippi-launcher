0f1cb3f6671152e9a654d881b80caba15387da5c
Vince Au <vinceau09@gmail.com>
2026-06-20 14:12:07 +0900

    fix async

--- .erb/scripts/electron-rebuild.js
+++ .erb/scripts/electron-rebuild.js
@@ -4,10 +4,12 @@ import fs from "fs";
 import { dependencies } from "../../release/app/package.json";
 import webpackPaths from "../configs/webpack.paths";
 
-if (Object.keys(dependencies || {}).length > 0 && fs.existsSync(webpackPaths.appNodeModulesPath)) {
-  await rebuild({
-    buildPath: webpackPaths.appPath,
-    force: true,
-    types: ["prod", "dev", "optional"],
-  });
-}
+void (async () => {
+  if (Object.keys(dependencies || {}).length > 0 && fs.existsSync(webpackPaths.appNodeModulesPath)) {
+    await rebuild({
+      buildPath: webpackPaths.appPath,
+      force: true,
+      types: ["prod", "dev", "optional"],
+    });
+  }
+})();
